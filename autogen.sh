051ed026cea93473377dfe9dc82db7a412dd2d01
Brenden Matthews <brenden1@rty.ca>
2005-08-04 03:01:22 +0000

    made autogen somewhat sane
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky@52 7f574dfc-610e-0410-a909-a81674777703

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 
-aclocal-1.9
-autoheader-2.59
-automake-1.9 -a
-autoconf-2.59
+if [ -x aclocal-1.9 ]
+then
+	aclocal-1.9
+else
+	aclocal
+fi
+
+if [ -x autoheader-2.59 ]
+then
+	autoheader-2.59
+else
+	autoheader
+fi
+
+if [ -x automake-1.9 ]
+then
+	automake-1.9 -a
+else
+	automake
+fi
+
+if [ -x autoconf-2.59 ]
+then
+	autoconf-2.59
+else
+	autoconf
+fi
