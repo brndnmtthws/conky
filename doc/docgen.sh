f3aa44b7dc8e43577e118aecaaf8226638c90cdc
Jason Tan <jtan325@users.sourceforge.net>
2005-08-22 00:41:57 +0000

    changed docgen.sh to be more flexible
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky@142 7f574dfc-610e-0410-a909-a81674777703

--- doc/docgen.sh
+++ doc/docgen.sh
@@ -2,12 +2,16 @@
 # $Id$
 
 xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl docs.xml > docs.html
-if [ -x docbook2man ]
-then
-	docbook2man docs.xml
-else
-	docbook2x-man docs.xml
-fi
+
+db2x_xsltproc -s man docs.xml -o docs.mxml
+db2x_manxml docs.mxml
+
+#if [ -x docbook2man ]
+#then
+#	docbook2man docs.xml
+#else
+#	docbook2x-man docs.xml
+#fi
 gzip -f conky.1
 man ./conky.1.gz | col -b > README
 mv README ../
