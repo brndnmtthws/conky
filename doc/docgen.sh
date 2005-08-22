4342eddec5547d3f011388c4a00c28dc2a234ee6
Brenden Matthews <brenden1@rty.ca>
2005-08-22 00:44:31 +0000

    docgen should be happy now
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky@143 7f574dfc-610e-0410-a909-a81674777703

--- doc/docgen.sh
+++ doc/docgen.sh
@@ -6,12 +6,6 @@ xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl doc
 db2x_xsltproc -s man docs.xml -o docs.mxml
 db2x_manxml docs.mxml
 
-#if [ -x docbook2man ]
-#then
-#	docbook2man docs.xml
-#else
-#	docbook2x-man docs.xml
-#fi
 gzip -f conky.1
 man ./conky.1.gz | col -b > README
 mv README ../
