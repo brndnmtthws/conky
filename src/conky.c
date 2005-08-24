84f29d62fc76b1a2b1282b3c06697d1cc1be8031
Brenden Matthews <brenden1@rty.ca>
2005-08-24 06:29:53 +0000

    maximum_width
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky@161 7f574dfc-610e-0410-a909-a81674777703

--- src/conky.c
+++ src/conky.c
@@ -3032,7 +3032,7 @@ static void draw_line(char *s)
 
 			case BAR:
 				{
-					if (cur_x > maximum_width - text_start_x) {
+					if (cur_x > maximum_width - text_start_x && maximum_width > 0) {
 						break;
 					}
 					int h =
