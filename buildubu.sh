171d9c1f87629041d9fa8f0a0a711f81f015810b
Jason Tan <jtan325@users.sourceforge.net>
2005-09-11 23:13:10 +0000

    ok, 1.3.1 debiaining ready
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky@320 7f574dfc-610e-0410-a909-a81674777703

--- buildubu.sh
+++ buildubu.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-rm -rf ../conky-1.3.0* ../conky_*
+rm -rf ../conky-1.3.1* ../conky_*
 make distclean
 sh autogen.sh
 ./configure
 make dist
-cp -f conky-1.3.0.tar.gz ../conky_1.3.0.orig.tar.gz
+cp -f conky-1.3.1.tar.gz ../conky_1.3.1.orig.tar.gz
 cd ..
-tar xvzf conky_1.3.0.orig.tar.gz
-cd conky-1.3.0
+tar xvzf conky_1.3.1.orig.tar.gz
+cd conky-1.3.1
 #dh_make -s
 cp -r ../conky/debian .
 rm -rf debian/CVS
