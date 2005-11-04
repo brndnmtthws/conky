5f968c82bc0fd66714e7f12fa0dd55f35799f369
Jason Tan <jtan325@users.sourceforge.net>
2005-11-04 09:10:03 +0000

    small packaging stuff for 1.3.3
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky@374 7f574dfc-610e-0410-a909-a81674777703

--- buildubu.sh
+++ buildubu.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-rm -rf ../conky-1.3.1* ../conky_*
+rm -rf ../conky-1.3.3* ../conky_*
 make distclean
 sh autogen.sh
 ./configure
 make dist
-cp -f conky-1.3.1.tar.gz ../conky_1.3.1.orig.tar.gz
+cp -f conky-1.3.3.tar.gz ../conky_1.3.3.orig.tar.gz
 cd ..
-tar xvzf conky_1.3.1.orig.tar.gz
-cd conky-1.3.1
+tar xvzf conky_1.3.3.orig.tar.gz
+cd conky-1.3.3
 #dh_make -s
 cp -r ../conky/debian .
 rm -rf debian/CVS
