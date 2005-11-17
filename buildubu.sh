1b89febedbed3726cb1dfbf5625b6f4bdd79c5ae
Brenden Matthews <brenden1@rty.ca>
2005-11-17 00:28:32 +0000

    fixing some debian stuff
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky@413 7f574dfc-610e-0410-a909-a81674777703

--- buildubu.sh
+++ buildubu.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-rm -rf ../conky-1.3.3* ../conky_*
+rm -rf ../conky-1.3.4* ../conky_*
 make distclean
 sh autogen.sh
 ./configure
 make dist
-cp -f conky-1.3.3.tar.gz ../conky_1.3.3.orig.tar.gz
+cp -f conky-1.3.4.tar.gz ../conky_1.3.3.orig.tar.gz
 cd ..
-tar xvzf conky_1.3.3.orig.tar.gz
-cd conky-1.3.3
+tar xvzf conky_1.3.4.orig.tar.gz
+cd conky-1.3.4
 #dh_make -s
 cp -r ../conky/debian .
 rm -rf debian/CVS
