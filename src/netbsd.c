cee0fa005a36f5cc36aa80e7faf81e09874feea0
Philip Kovacs <pkovacs@users.sourceforge.net>
2005-11-17 04:54:45 +0000

    fixed typos
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky@419 7f574dfc-610e-0410-a909-a81674777703

--- src/netbsd.c
+++ src/netbsd.c
@@ -347,7 +347,7 @@ int open_acpi_temperature(const char *name)
 
 void get_acpi_ac_adapter( char * p_client_buffer, size_t client_buffer_size )
 {
-	if ( !p_client_buffer !! client_buffer_size <= 0 )
+	if ( !p_client_buffer || client_buffer_size <= 0 )
 		return;
 
 	/* not implemented */
@@ -359,7 +359,7 @@ void get_acpi_ac_adapter( char * p_client_buffer, size_t client_buffer_size )
 /*char *get_acpi_fan()*/
 void get_acpi_fan( char * p_client_buffer, size_t client_buffer_size )
 {
-	if ( !p_client_buffer !! client_buffer_size <= 0 )
+	if ( !p_client_buffer || client_buffer_size <= 0 )
 		return;
 
 	/* not implemented */
