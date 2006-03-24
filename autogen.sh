435842e4941554718c2107d60ac8ccafefef1490
Jasper <jasper_la@users.sourceforge.net>
2006-03-24 14:30:36 +0000

    get autogen.sh to detect the autotools correctly on FreeBSD. From:	"Petr Holub" <hopet@users.sourceforge.net>
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky1@608 7f574dfc-610e-0410-a909-a81674777703

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 # $Id$
 
-aclocal-1.9
-libtoolize --force
-autoheader-2.59
-automake-1.9 -a
-autoconf-2.59
+if [ `uname -s` = FreeBSD ]; then
+	aclocal19
+	libtoolize15 --force
+	autoheader259
+	automake19 -a
+	autoconf259
+else
+	aclocal-1.9
+	libtoolize --force
+	autoheader-2.59
+	automake-1.9 -a
+	autoconf-2.59
+fi
