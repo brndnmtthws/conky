0411537dce7d7f6e2af3e21d3cd0a1af2082a506
Philip Kovacs <pkovacs@users.sourceforge.net>
2006-11-03 22:33:27 +0000

    subversion Id keyword
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky1@717 7f574dfc-610e-0410-a909-a81674777703

--- src/audacious.h
+++ src/audacious.h
@@ -3,7 +3,7 @@
  * 
  * Copyright (C) 2005  Philip Kovacs kovacsp3@comcast.net
  *
- * $Id $
+ * $Id$
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
