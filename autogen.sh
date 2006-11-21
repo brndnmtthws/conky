3cc60c2dc2dc067fa86dbe4dde28a2eafaae65e7
Philip Kovacs <pkovacs@users.sourceforge.net>
2006-11-21 07:31:42 +0000

    autotool changes
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky1@786 7f574dfc-610e-0410-a909-a81674777703

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 # $Id$
 
+# autogen.sh
+# optional $1 = full path to svn local working repository
+
 AUTOCONF=${AUTOCONF:-autoconf}
 AUTOMAKE=${AUTOMAKE:-automake}
 ACLOCAL=${ACLOCAL:-aclocal}
 AUTOHEADER=${AUTOHEADER:-autoheader}
 LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
+WORKINGREPO=${1:-.}
 
-$ACLOCAL
-$LIBTOOLIZE --force
-$AUTOHEADER
-$AUTOMAKE -a
-$AUTOCONF
+# substitute svn revision
+revision=`LC_ALL=C svn info ${WORKINGREPO} | awk '/^Revision: / {printf "%05d\n", $2}'`
+sed -e "s/@REVISION@/${revision}/g" \
+    < "configure.ac.in" > "configure.ac"
+
+echo Running $ACLOCAL ... && $ACLOCAL
+echo Running $LIBTOOLIZE --force ... && $LIBTOOLIZE --force
+echo Running $AUTOHEADER ... && $AUTOHEADER
+echo Running $AUTOMAKE -a ... && $AUTOMAKE -a
+echo Running $AUTOCONF ... && $AUTOCONF
