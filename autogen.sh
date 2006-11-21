d294df837d8e70082e2ddff4ebf81d79ba7357a6
Philip Kovacs <pkovacs@users.sourceforge.net>
2006-11-21 20:47:05 +0000

    added autogen.sh clean to remove configure.ac
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky1@789 7f574dfc-610e-0410-a909-a81674777703

--- autogen.sh
+++ autogen.sh
@@ -2,17 +2,21 @@
 # $Id$
 
 # autogen.sh
-# optional $1 = full path to svn local working repository
+# optional $1 = full path to svn working copy or "clean"
 
 AUTOCONF=${AUTOCONF:-autoconf}
 AUTOMAKE=${AUTOMAKE:-automake}
 ACLOCAL=${ACLOCAL:-aclocal}
 AUTOHEADER=${AUTOHEADER:-autoheader}
 LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
-WORKINGREPO=${1:-.}
+
+if [ "$1" == "clean" ]; then
+    /bin/rm -f configure.ac
+    exit 0
+fi
 
 # substitute svn revision
-revision=`LC_ALL=C svn info ${WORKINGREPO} | awk '/^Revision: / {printf "%05d\n", $2}'`
+revision=`LC_ALL=C svn info $1 | awk '/^Revision: / {printf "%05d\n", $2}'`
 sed -e "s/@REVISION@/${revision}/g" \
     < "configure.ac.in" > "configure.ac"
 
