1d139aebfb046166222ad4b78198242fd0e7e9c8
Philip Kovacs <pkovacs@users.sourceforge.net>
2007-08-28 20:37:42 +0000

    autogen.sh improvements
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky1@930 7f574dfc-610e-0410-a909-a81674777703

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,8 @@
 # $Id$
 
 # autogen.sh
-# optional $1 = full path to svn working copy or "clean"
+#
+# optional $1 = optional directory containing build tree or svn working copy
 
 export AUTOCONF=${AUTOCONF:-autoconf}
 export AUTOMAKE=${AUTOMAKE:-automake}
@@ -10,15 +11,16 @@ export ACLOCAL=${ACLOCAL:-aclocal}
 export AUTOHEADER=${AUTOHEADER:-autoheader}
 export LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
 
-if [ "$1" = "clean" ]; then
-    /bin/rm -f configure.ac
-    exit 0
-fi
+# identify svn revision, if an svn working copy
+if test "$1" != "" && test -d "$1/.svn"; then
+    revision=`LC_ALL=C svn info $1 | awk '/^Revision: / {printf "%05d\n", $2}'`;
+elif test -d ".svn"; then
+    revision=`LC_ALL=C svn info | awk '/^Revision: / {printf "%05d\n", $2}'`; 
+else
+    revision="NONE"; fi
 
-# substitute svn revision
-revision=`LC_ALL=C svn info $1 | awk '/^Revision: / {printf "%05d\n", $2}'`
-sed -e "s/@REVISION@/${revision}/g" \
-    < "configure.ac.in" > "configure.ac"
+# generate configure.ac with substituted svn revision
+sed -e "s/@REVISION@/${revision}/g" < "configure.ac.in" > "configure.ac"
 
 echo Running $ACLOCAL -I m4 ... && $ACLOCAL -I m4
 echo Running $LIBTOOLIZE --force --copy ... && $LIBTOOLIZE --force --copy
