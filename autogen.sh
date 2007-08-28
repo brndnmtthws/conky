0959e270ba1e51b74c3c60fd3d169aa7ad608c91
Philip Kovacs <pkovacs@users.sourceforge.net>
2007-08-28 20:27:45 +0000

    add pkg.m4 to fix pkg-config catch-22
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky1@929 7f574dfc-610e-0410-a909-a81674777703

--- autogen.sh
+++ autogen.sh
@@ -4,11 +4,11 @@
 # autogen.sh
 # optional $1 = full path to svn working copy or "clean"
 
-AUTOCONF=${AUTOCONF:-autoconf}
-AUTOMAKE=${AUTOMAKE:-automake}
-ACLOCAL=${ACLOCAL:-aclocal}
-AUTOHEADER=${AUTOHEADER:-autoheader}
-LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
+export AUTOCONF=${AUTOCONF:-autoconf}
+export AUTOMAKE=${AUTOMAKE:-automake}
+export ACLOCAL=${ACLOCAL:-aclocal}
+export AUTOHEADER=${AUTOHEADER:-autoheader}
+export LIBTOOLIZE=${LIBTOOLIZE:-libtoolize}
 
 if [ "$1" = "clean" ]; then
     /bin/rm -f configure.ac
@@ -20,7 +20,7 @@ revision=`LC_ALL=C svn info $1 | awk '/^Revision: / {printf "%05d\n", $2}'`
 sed -e "s/@REVISION@/${revision}/g" \
     < "configure.ac.in" > "configure.ac"
 
-echo Running $ACLOCAL ... && $ACLOCAL
+echo Running $ACLOCAL -I m4 ... && $ACLOCAL -I m4
 echo Running $LIBTOOLIZE --force --copy ... && $LIBTOOLIZE --force --copy
 echo Running $AUTOHEADER ... && $AUTOHEADER
 echo Running $AUTOMAKE --add-missing --copy --gnu ... && $AUTOMAKE --add-missing --copy --gnu
