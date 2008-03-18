b77da1255a79e697d6bd9195fe7a1e82a0760c0d
Brenden Matthews <brenden1@rty.ca>
2008-03-18 17:08:22 +0000

    updating new file props
    git-svn-id: https://conky.svn.sourceforge.net/svnroot/conky/trunk/conky1@1011 7f574dfc-610e-0410-a909-a81674777703

--- src/users.c
+++ src/users.c
@@ -23,7 +23,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * $Id: conky.h 1007 2008-02-20 20:30:45Z IQgryn $
+ * $Id$
  *
  */
 
