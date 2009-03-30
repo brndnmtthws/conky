6071fc4f45b7212c5ae50975c47f563003aa419a
Brenden Matthews <brenden@rty.ca>
2009-03-29 22:55:30 -0600

    Update copyright stuff, fix conky.conf weirdness.

--- src/freebsd.c
+++ src/freebsd.c
@@ -6,7 +6,7 @@
  *
  * Please see COPYING for details
  *
- * Copyright (c) 2005-2008 Brenden Matthews, Philip Kovacs, et. al.
+ * Copyright (c) 2005-2009 Brenden Matthews, Philip Kovacs, et. al.
  *	(see AUTHORS)
  * All rights reserved.
  *
