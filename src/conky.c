f8a6a4035ced2ac48582581017d7385c39785a35
Yeon-Hyeong Yang <lbird@users.sourceforge.net>
2009-05-03 21:23:59 -0600

    Added Sony VAIO fanspeed info (thanks Yeon-Hyeong).
    Also removed the README and man page from the repo, as they don't really
    need to be there and just cause conflicts.

--- src/conky.c
+++ src/conky.c
@@ -1223,6 +1223,9 @@ static struct text_object *construct_text_object(const char *s,
 	END OBJ(ibm_volume, 0)
 	END OBJ(ibm_brightness, 0)
 #endif
+	/* information from sony_laptop kernel module
+	 * /sys/devices/platform/sony-laptop */
+	END OBJ(sony_fanspeed, 0)
 	END OBJ_IF(if_gw, 0)
 	END OBJ(ioscheduler, 0)
 		if (!arg) {
@@ -3415,6 +3418,11 @@ static void generate_text_internal(char *p, int p_max_size,
 				get_ibm_acpi_brightness(p, p_max_size);
 			}
 #endif /* IBM */
+			/* information from sony_laptop kernel module
+			 * /sys/devices/platform/sony-laptop */
+			OBJ(sony_fanspeed) {
+				get_sony_fanspeed(p, p_max_size);
+			}
 			OBJ(if_gw) {
 				if (!cur->gw_info.count) {
 					DO_JUMP;
