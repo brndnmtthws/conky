9ce2c8ba0734979fbc41c00fd6ca4287c2ec0af4
Brenden Matthews <brenden@rty.ca>
2009-06-13 13:40:27 -0600

    Fix regression in Lua inotify stuff.

--- src/llua.c
+++ src/llua.c
@@ -53,7 +53,7 @@ void llua_load(const char *script)
 		lua_pop(lua_L, 1);
 #ifdef HAVE_SYS_INOTIFY_H
 	} else if (!llua_block_notify && inotify_fd != -1) {
-		llua_append_notify(script);
+		llua_append_notify(path);
 #endif /* HAVE_SYS_INOTIFY_H */
 	}
 }
