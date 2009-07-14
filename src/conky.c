brndnmtthws/conky

commit 4a14668772a37b18a9aceb8951ece260084bc96e

Fix a part of the memleaks in the X-related code, if you remove all lines above TEXT in defconfig.h, then it should run without memleaks.

src/conky.c
@@ -7373,10 +7373,6 @@ static void main_loop(void)
 		inotify_fd = inotify_config_wd = 0;
 	}
 #endif /* HAVE_SYS_INOTIFY_H */
-
-#ifdef X11
-	X11_destroy_window();
-#endif /* X11 */
 }
 
 static void load_config_file(const char *);
@@ -7470,25 +7466,14 @@ void clean_up(void)
 	}
 #ifdef X11
 	if (x_initialised == YES) {
-#ifdef HAVE_XDBE
-		if (use_xdbe) {
-			XdbeDeallocateBackBufferName(display, window.back_buffer);
-		}
-#endif
-#ifdef OWN_WINDOW
-		if (own_window) {
-			XDestroyWindow(display, window.window);
-			XClearWindow(display, RootWindow(display, screen));
-			XFlush(display);
-		} else
-#endif
-		{
-			XClearWindow(display, RootWindow(display, screen));
-			clear_text(1);
-			XFlush(display);
-		}
-
 		free_fonts();
+		if(x11_stuff.region) {
+			XDestroyRegion(x11_stuff.region);
+			x11_stuff.region = NULL;
+		}
+		destroy_window();
+		XClearWindow(display, RootWindow(display, screen));
+		XCloseDisplay(display);
 	}else{
 		free(fonts);	//in set_default_configurations a font is set but not loaded
 	}

commit cd92949e6704b38241666e26dc38eae6c9ba6d9c

Fix strange effects on desktop caused by 4a14668772a37b18a9aceb8951ece260084bc96e

src/conky.c
@@ -7478,12 +7478,12 @@ void clean_up(void)
 	}
 #ifdef X11
 	if (x_initialised == YES) {
+		destroy_window();
 		free_fonts();
 		if(x11_stuff.region) {
 			XDestroyRegion(x11_stuff.region);
 			x11_stuff.region = NULL;
 		}
-		destroy_window();
 		XClearWindow(display, RootWindow(display, screen));
 		XCloseDisplay(display);
 		if(info.x11.desktop.all_names) {

src/x11.c
@@ -194,7 +194,6 @@ void set_transparent_background(Window win)
 
 void destroy_window(void)
 {
-	XDestroyWindow(display, window.window);
 	XFreeGC(display, window.gc);
 	memset(&window, 0, sizeof(struct conky_window));
 }