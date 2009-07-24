ca04b9a3c7e354b36cd18f9b71c7f3e67a355d28
Nikolas Garofil <garo@dunaldi.garofil.be>
2009-07-24 16:17:03 +0200

    just making bdf5e87c927348a6cf446e2840b2b14ad46a7dbc easier to read

--- src/rss.c
+++ src/rss.c
@@ -65,7 +65,8 @@ void rss_process_info(char *p, int p_max_size, char *uri, char *action, int
 		snprintf(p, p_max_size, "prss: Error reading RSS data\n");
 	} else {
 		if (strcmp(action, "feed_title") == EQUAL) {
-		        if (str = data->title) {
+		        str = data->title;
+		        if (str != NULL) {
 			        // remove trailing new line if one exists
 			        if (str[strlen(str) - 1] == '\n') {
 				        str[strlen(str) - 1] = 0;
