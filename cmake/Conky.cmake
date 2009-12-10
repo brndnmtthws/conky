01c01671e588c90542994fa57e1a47d57910bea2
Brenden Matthews <brenden@diddyinc.com>
2009-12-10 15:19:45 -0800

    Require GCC >=4.4.

--- cmake/Conky.cmake
+++ cmake/Conky.cmake
@@ -1,5 +1,14 @@
 # vim: ts=4 sw=4 noet ai cindent syntax=cmake
 
+# Conky 2.x requires GCC 4.4 or newer
+try_compile(GCC4_WORKS
+	${CMAKE_CURRENT_BINARY_DIR}
+	${CMAKE_MODULE_PATH}/gcc44test.cc
+)
+if(NOT GCC4_WORKS)
+	message(FATAL_ERROR "Conky 2.x requires GCC 4.4.0 or newer")
+endif(NOT GCC4_WORKS)
+
 # Set system vars
 if(CMAKE_SYSTEM_NAME MATCHES "Linux")
 	set(OS_LINUX true)
