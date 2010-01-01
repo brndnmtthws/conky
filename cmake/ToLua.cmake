2951f5571c15acf20a20750a6c9accd699c3df82
Brenden Matthews <brenden@diddyinc.com>
2010-01-01 15:49:41 -0800

    Add missing copyright notices.

--- cmake/ToLua.cmake
+++ cmake/ToLua.cmake
@@ -1,3 +1,25 @@
+# vim: ts=4 sw=4 noet ai cindent syntax=cmake
+#
+# Conky, a system monitor, based on torsmo
+#
+# Please see COPYING for details
+#
+# Copyright (c) 2005-2010 Brenden Matthews, et. al. (see AUTHORS)
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 function(wrap_tolua VAR)
 if(NOT ARGN)
 	message(SEND_ERROR "Error: wrap_tolua called without any files")
