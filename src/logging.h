caf38b3a120b40ff7b738a532cfd122eb2de9d91
Nikolas Garofil <nikolas@garofil.be>
2010-02-18 01:34:19 +0100

    Fix current_mail_spool memleak

--- src/logging.h
+++ src/logging.h
@@ -27,6 +27,8 @@
  *
  */
 
+#include "mail.h"
+
 void clean_up(void *memtofree1, void* memtofree2);
 
 #ifndef _LOGGING_H
@@ -40,7 +42,7 @@ void clean_up(void *memtofree1, void* memtofree2);
 
 /* critical error */
 #define CRIT_ERR(memtofree1, memtofree2, ...) \
-	{ NORM_ERR(__VA_ARGS__); clean_up(memtofree1, memtofree2); exit(EXIT_FAILURE); }
+	{ NORM_ERR(__VA_ARGS__); clean_up(memtofree1, memtofree2); free(current_mail_spool); exit(EXIT_FAILURE); }
 
 /* debugging output */
 extern int global_debug_level;
