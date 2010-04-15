36ebcf347de0faf57841585118f82ef95e5f1e82
Nikolas Garofil <nikolas@garofil.be>
2010-04-15 19:56:14 +0200

    Bugfix: read_tcp treated a succesfull connection as failed and vice versa

--- src/read_tcp.c
+++ src/read_tcp.c
@@ -87,7 +87,7 @@ void print_read_tcp(struct text_object *obj, char *p, int p_max_size)
 	addr.sin_family = AF_INET;
 	addr.sin_port = rtd->port;
 	memcpy(&addr.sin_addr, he->h_addr, he->h_length);
-	if (!connect(sock, (struct sockaddr*)&addr, sizeof(struct sockaddr))) {
+	if (connect(sock, (struct sockaddr*)&addr, sizeof(struct sockaddr)) != 0) {
 		NORM_ERR("read_tcp: Couldn't create a connection");
 		return;
 	}
