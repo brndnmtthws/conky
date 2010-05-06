acbb531adf122bef037fc47e81c7da9314876014
Nikolas Garofil <nikolas@garofil.be>
2010-05-06 12:00:43 +0200

    Bugfix: RSS interval was in secs instead of mins

--- src/rss.c
+++ src/rss.c
@@ -67,7 +67,7 @@ static void rss_process_info(char *p, int p_max_size, char *uri, char *action, i
 		curloc->result = malloc(sizeof(PRSS));
 		memset(curloc->result, 0, sizeof(PRSS));
 		curloc->process_function = &prss_parse_data;
-		ccurl_init_thread(curloc, interval);
+		ccurl_init_thread(curloc, interval * 60);
 		if (!curloc->p_timed_thread) {
 			NORM_ERR("error setting up RSS thread");
 		}
