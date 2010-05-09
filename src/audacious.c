812107391272943d7a9823d9203f32ae8db5a64d
Nikolas Garofil <nikolas@garofil.be>
2010-05-09 22:52:27 +0200

    Make sure audacious-code compiles

--- src/audacious.c
+++ src/audacious.c
@@ -62,7 +62,7 @@ static audacious_t audacious_items;
 /* -----------------------------------------
  * Conky update function for audacious data.
  * ----------------------------------------- */
-void update_audacious(void)
+int update_audacious(void)
 {
 	/* The worker thread is updating audacious_items array asynchronously
 	 * to the main conky thread.
@@ -79,6 +79,7 @@ void update_audacious(void)
 	timed_thread_lock(info.audacious.p_timed_thread);
 	memcpy(&info.audacious.items, audacious_items, sizeof(audacious_items));
 	timed_thread_unlock(info.audacious.p_timed_thread);
+	return 0;
 }
 
 /* ---------------------------------------------------------
