4256da3b7f11256f8959c7e812ebce8070fdbb77
Nikolas Garofil <nikolas@garofil.be>
2010-05-09 22:42:06 +0200

    forgot to change header in previous commit

--- src/audacious.h
+++ src/audacious.h
@@ -60,7 +60,7 @@ int create_audacious_thread(void);
 int destroy_audacious_thread(void);
 
 /* Service routine for the conky main thread */
-void update_audacious(void);
+int update_audacious(void);
 
 /* Thread functions */
 void audacious_thread_func(thread_handle &handle);
