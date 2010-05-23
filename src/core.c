8564ff6610cf5ed060e6dd206ad9878e96d98130
Cesare Tirabassi <norsetto@ubuntu.com>
2010-05-23 11:08:08 +0200

    Fix battery_bar not parsing arguments correctly (sf: #2998034, lp: #569195)

--- src/core.c
+++ src/core.c
@@ -277,12 +277,13 @@ struct text_object *construct_text_object(const char *s, const char *arg, long
 		obj->data.s = strndup(bat, text_buffer_size);
 	END OBJ(battery_bar, 0)
 		char bat[64];
-		if (arg) {
+
+		arg = scan_bar(obj, arg);
+		if (arg && strlen(arg)>0) {
 			sscanf(arg, "%63s", bat);
 		} else {
 			strcpy(bat, "BAT0");
 		}
-		scan_bar(obj, bat);
 		obj->data.s = strndup(bat, text_buffer_size);
 #endif /* !__OpenBSD__ */
 
