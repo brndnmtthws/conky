56ddea77a80114dab6542923fc2b09ac00442c39
Kevin Lyles <kevinlyles@gmail.com>
2010-08-05 21:34:06 -0500

    Fixed foreground colors being semi-transparent
    Signed-off-by: Brenden Matthews <brenden@diddyinc.com>

--- src/conky.cc
+++ src/conky.cc
@@ -1547,12 +1547,12 @@ int draw_each_line_inner(char *s, int special_index, int last_special_applied)
 										  );
 								}
 #endif /* DEBUG_lol */
-								XSetForeground(display, window.gc, tmpcolour[
+										set_foreground_color(tmpcolour[
 										(int)((float)(w - 2) - current->graph[j] *
 											(w - 2) / (float)current->scale)
 										]);
 							} else {
-								XSetForeground(display, window.gc, tmpcolour[colour_idx++]);
+								set_foreground_color(tmpcolour[colour_idx++]);
 							}
 						}
 						/* this is mugfugly, but it works */
