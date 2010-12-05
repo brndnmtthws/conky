3b5d5f33b3b665e9f7bfb74f3b144876d3dc60ef
Nikos Ntarmos <ntarmos@cs.uoi.gr>
2010-12-05 05:57:36 +0200

    Build fixes for bmpx
    Signed-off-by: Nikos Ntarmos <ntarmos@cs.uoi.gr>
    Signed-off-by: Pavel Labath <pavelo@centrum.sk>

--- src/bmpx.cc
+++ src/bmpx.cc
@@ -42,7 +42,7 @@ static DBusGProxy *remote_object;
 static int connected = 0;
 static char *unknown = "unknown";
 
-void fail(GError *error);
+void fail(GError *error, struct information *);
 
 void update_bmpx()
 {
@@ -58,15 +58,15 @@ void update_bmpx()
 		bus = dbus_g_bus_get(DBUS_BUS_SESSION, &error);
 		if (bus == NULL) {
 			NORM_ERR("BMPx error 1: %s\n", error->message);
-			fail(error);
+			fail(error, current_info);
 			return;
 		}
 
 		remote_object = dbus_g_proxy_new_for_name(bus, BMP_DBUS_SERVICE,
-				BMP_DBUS_PATH, BMP_DBUS_INTERFACE);
+				BMP_DBUS_PATH__BMP, BMP_DBUS_INTERFACE__BMP);
 		if (!remote_object) {
 			NORM_ERR("BMPx error 2: %s\n", error->message);
-			fail(error);
+			fail(error, current_info);
 			return;
 		}
 
@@ -78,7 +78,7 @@ void update_bmpx()
 					G_TYPE_INVALID, G_TYPE_INT, &current_track, G_TYPE_INVALID)) {
 		} else {
 			NORM_ERR("BMPx error 3: %s\n", error->message);
-			fail(error);
+			fail(error, current_info);
 			return;
 		}
 
@@ -103,17 +103,17 @@ void update_bmpx()
 				g_value_get_string(g_hash_table_lookup(metadata, "location"));
 		} else {
 			NORM_ERR("BMPx error 4: %s\n", error->message);
-			fail(error);
+			fail(error, current_info);
 			return;
 		}
 
 		g_hash_table_destroy(metadata);
 	} else {
-		fail(error);
+		fail(error, current_info);
 	}
 }
 
-void fail(GError *error)
+void fail(GError *error, struct information *current_info)
 {
 	if (error) {
 		g_error_free(error);
