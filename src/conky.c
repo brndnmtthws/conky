brndnmtthws/conky

commit 951cb1ac7e34347f54dc968e68371d5c334abc5e

Fix graph updating for large (>512pix) graphs (sf.net #3253656)
apparently, this was actually a feature as someone made it deliberately act that way. However, I
agree with the bug reporter that it is strange so I remove it.

src/specials.h
@@ -34,8 +34,6 @@
 
 #define SPECIAL_CHAR '\x01'
 
-#define MAX_GRAPH_DEPTH 512
-
 // don't use spaces in LOGGRAPH or NORMGRAPH if you change them
 #define LOGGRAPH "-l"
 #define TEMPGRAD "-t"
@@ -67,7 +65,6 @@ struct special_t {
 	double *graph;
 	double scale;			/* maximum value */
 	short show_scale;
-	int graph_width;
 	int scaled;			/* auto adjust maximum */
 	unsigned long first_colour;	// for graph gradient
 	unsigned long last_colour;

src/conky.cc
@@ -1580,11 +1580,7 @@ int draw_each_line_inner(char *s, int special_index, int last_special_applied)
 								cur_x + i + 1, by + h, cur_x + i + 1,
 								round_to_int((double)by + h - current->graph[j] *
 									(h - 1) / current->scale));
-						if ((w - i) / ((float) (w - 2) /
-									(current->graph_width)) > j
-								&& j < MAX_GRAPH_DEPTH - 3) {
-							j++;
-						}
+						j++;
 					}
 					free_and_zero(tmpcolour);
 					if (h > cur_y_add

src/specials.cc
@@ -392,13 +392,13 @@ static void graph_append(struct special_t *graph, double f, char showaslog)
 	}
 
 	/* shift all the data by 1 */
-	for (i = graph->graph_width - 1; i > 0; i--) {
+	for (i = graph->width - 1; i > 0; i--) {
 		graph->graph[i] = graph->graph[i - 1];
 	}
 	graph->graph[0] = f;	/* add new data */
 	
 	if(graph->scaled) {
-		graph->scale = *std::max_element(graph->graph + 0, graph->graph + graph->graph_width);
+		graph->scale = *std::max_element(graph->graph + 0, graph->graph + graph->width);
 		if(graph->scale < 1e-47) {
 			/* avoid NaN's when the graph is all-zero (e.g. before the first update)
 			 * there is nothing magical about 1e-47 here */
@@ -422,14 +422,8 @@ void new_graph(struct text_object *obj, char *buf, int buf_max_size, double val)
 
 	s->width = g->width;
 	if (s->graph == NULL) {
-		if (s->width > 0 && s->width < MAX_GRAPH_DEPTH) {
-			// subtract 2 for the box
-			s->graph_width = s->width /* - 2 */;
-		} else {
-			s->graph_width = MAX_GRAPH_DEPTH - 2;
-		}
-		s->graph = (double*)malloc(s->graph_width * sizeof(double));
-		memset(s->graph, 0, s->graph_width * sizeof(double));
+		s->graph = (double*)malloc(s->width * sizeof(double));
+		memset(s->graph, 0, s->width * sizeof(double));
 		s->scale = 100;
 	}
 	s->height = g->height;
@@ -445,9 +439,6 @@ void new_graph(struct text_object *obj, char *buf, int buf_max_size, double val)
 		s->show_scale = 1;
 	}
 	s->tempgrad = g->tempgrad;
-	/* if (s->width) {
-		s->graph_width = s->width - 2;	// subtract 2 for rectangle around
-	} */
 #ifdef MATH
 	if (g->flags & SF_SHOWLOG) {
 		s->scale = log10(s->scale + 1);

commit c1277d650bff74d338e001c80a77a449450fd8e9

Fix for regression introduced by 951cb1ac7e34347f54dc968e68371d5c334abc5e.

src/conky.c
@@ -2994,6 +2994,7 @@ int draw_each_line_inner(char *s, int special_index, int last_special_applied)
 					w = specials[special_index].width;
 					if (w == 0) {
 						w = text_start_x + text_width - cur_x - 1;
+						specials[special_index].graph_width = w - 1;
 					}
 					if (w < 0) {
 						w = 0;
@@ -3007,52 +3008,58 @@ int draw_each_line_inner(char *s, int special_index, int last_special_applied)
 					XSetLineAttributes(display, window.gc, 1, LineSolid,
 						CapButt, JoinMiter);
 
-					if (specials[special_index].last_colour != 0
-							|| specials[special_index].first_colour != 0) {
-						tmpcolour = do_gradient(w - 1, specials[special_index].last_colour, specials[special_index].first_colour);
-					}
-					colour_idx = 0;
-					for (i = w - 2; i > -1; i--) {
+					/* in case we don't have a graph yet */
+					if (specials[special_index].graph) {
+
 						if (specials[special_index].last_colour != 0
 								|| specials[special_index].first_colour != 0) {
-							if (specials[special_index].tempgrad) {
+							tmpcolour = do_gradient(w - 1,
+									specials[special_index].last_colour,
+									specials[special_index].first_colour);
+						}
+						colour_idx = 0;
+						for (i = w - 2; i > -1; i--) {
+							if (specials[special_index].last_colour != 0
+									|| specials[special_index].first_colour != 0) {
+								if (specials[special_index].tempgrad) {
 #ifdef DEBUG_lol
-								assert(
-										(int)((float)(w - 2) - specials[special_index].graph[j] *
-											(w - 2) / (float)specials[special_index].graph_scale)
-										< w - 1
-									  );
-								assert(
-										(int)((float)(w - 2) - specials[special_index].graph[j] *
-											(w - 2) / (float)specials[special_index].graph_scale)
-										> -1
-									  );
-								if (specials[special_index].graph[j] == specials[special_index].graph_scale) {
 									assert(
 											(int)((float)(w - 2) - specials[special_index].graph[j] *
 												(w - 2) / (float)specials[special_index].graph_scale)
-											== 0
+											< w - 1
 										  );
-								}
+									assert(
+											(int)((float)(w - 2) - specials[special_index].graph[j] *
+												(w - 2) / (float)specials[special_index].graph_scale)
+											> -1
+										  );
+									if (specials[special_index].graph[j] == specials[special_index].graph_scale) {
+										assert(
+												(int)((float)(w - 2) - specials[special_index].graph[j] *
+													(w - 2) / (float)specials[special_index].graph_scale)
+												== 0
+											  );
+									}
 #endif /* DEBUG_lol */
-										set_foreground_color(tmpcolour[
-												(int)((float)(w - 2) -
-													specials[special_index].graph[j]
-													* (w - 2) /
-													(float)specials[special_index].graph_scale)
-												]);
-							} else {
-								set_foreground_color(tmpcolour[colour_idx++]);
+									set_foreground_color(tmpcolour[
+											(int)((float)(w - 2) -
+												specials[special_index].graph[j]
+												* (w - 2) /
+												(float)specials[special_index].graph_scale)
+											]);
+								} else {
+									set_foreground_color(tmpcolour[colour_idx++]);
+								}
 							}
+							/* this is mugfugly, but it works */
+							XDrawLine(display, window.drawable, window.gc,
+									cur_x + i + 1, by + h, cur_x + i + 1,
+									round_to_int((double)by + h - specials[special_index].graph[j] *
+										(h - 1) / specials[special_index].graph_scale));
+							++j;
 						}
-						/* this is mugfugly, but it works */
-						XDrawLine(display, window.drawable, window.gc,
-								cur_x + i + 1, by + h, cur_x + i + 1,
-								round_to_int((double)by + h - specials[special_index].graph[j] *
-									(h - 1) / specials[special_index].graph_scale));
-						++j;
+						if (tmpcolour) free(tmpcolour);
 					}
-					if (tmpcolour) free(tmpcolour);
 					if (h > cur_y_add
 							&& h > font_h) {
 						cur_y_add = h;

src/specials.c
@@ -353,6 +353,9 @@ static void graph_append(struct special_t *graph, double f, char showaslog)
 {
 	int i;
 
+	/* do nothing if we don't even have a graph yet */
+	if (!graph->graph) return;
+
 	if (showaslog) {
 #ifdef MATH
 		f = log10(f + 1);
@@ -364,7 +367,7 @@ static void graph_append(struct special_t *graph, double f, char showaslog)
 	}
 
 	/* shift all the data by 1 */
-	for (i = graph->width - 1; i > 0; i--) {
+	for (i = graph->graph_allocated - 1; i > 0; i--) {
 		graph->graph[i] = graph->graph[i - 1];
 		if (graph->scaled && graph->graph[i - 1] > graph->graph_scale) {
 			/* check if we need to update the scale */
@@ -392,10 +395,25 @@ void new_graph(struct text_object *obj, char *buf, int buf_max_size, double val)
 	s = new_special(buf, GRAPH);
 
 	s->width = g->width;
-	if (s->graph == NULL) {
-		s->graph = malloc(s->width * sizeof(double));
-		memset(s->graph, 0, s->width * sizeof(double));
-		s->graph_scale = 100;
+	if (s->width) s->graph_width = s->width;
+
+	if (s->graph_width != s->graph_allocated) {
+		char *graph = realloc(s->graph, s->graph_width * sizeof(double));
+		DBGP("reallocing graph from %d to %d", s->graph_allocated, s->graph_width);
+		if (!s->graph) {
+			/* initialize */
+			memset(graph, 0, s->graph_width * sizeof(double));
+			s->graph_scale = 100;
+		} else {
+			if (s->graph_width > s->graph_allocated) {
+				/* initialize the new region */
+				memset(graph + (s->graph_allocated * sizeof(double)), 0,
+						(s->graph_width - s->graph_allocated) *
+						sizeof(double));
+			}
+		}
+		s->graph = graph;
+		s->graph_allocated = s->graph_width;
 	}
 	s->height = g->height;
 	s->first_colour = adjust_colours(g->first_colour);

src/specials.h
@@ -64,6 +64,8 @@ struct special_t {
 	double *graph;
 	double graph_scale;
 	short show_scale;
+	int graph_width;
+	int graph_allocated;
 	int scaled;
 	unsigned long first_colour;	// for graph gradient
 	unsigned long last_colour;