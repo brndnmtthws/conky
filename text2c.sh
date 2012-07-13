ef6282fedca1e3c3bf08739f82d3a75055393081
Pavel Labath <pavelo@centrum.sk>
2012-07-13 20:13:36 +0200

    Remove \t from sed script
    apparently, non-GNU implementations of sed do not understand backslash-sequences.

--- text2c.sh
+++ text2c.sh
@@ -36,5 +36,5 @@ outupper="`basename "$2" | tr '[a-z-.]' '[A-Z__]'`"
 
 (
 	printf "const char %s[] = \n" "$3"
-	sed -e 's/["\]/\\&/g' -e 's/^/\t"/' -e 's/$/\\n"/' -e '$s/$/;/' "$1"
+	sed -e 's/["\]/\\&/g' -e 's/^/    "/' -e 's/$/\\n"/' -e '$s/$/;/' "$1"
 ) > "$2"
