brndnmtthws/conky

commit 8827d6158e13e222a3e392f6c01a8c78088dfb7b

Fix an off-by-one in top.c (sf.net #3538234)
bug reported by arclance

src/top.c
@@ -703,7 +703,7 @@ static int insert_sp_element(struct sorted_process *sp_cur,
 		sp_cur->greater = *p_sp_tail;
 		*p_sp_tail = sp_cur;
 		did_insert = x;
-	} else if (x >= max_elements) {
+	} else if (x > max_elements) {
 		/* We inserted an element and now the list is too big by one.
 		 * Destroy the smallest element */
 		sp_destroy = *p_sp_tail;

commit 1c1feb7db2b5e5c6fe44e221d6a031a6dbf7e851

Fix a memory leak in top.c (sf.net #3543857)
The leak was caused by commit 8827d61, attempting to solve a problem with top displaying 10th
process incorrectly.

I think there should be a more elegant solution to this, but I don't know how to find it atm
(it could be due the fact that it's 4am here).

src/top.c
@@ -725,8 +725,11 @@ static void sp_acopy(struct sorted_process *sp_head, struct process **ar, int ma
 	int x;
 
 	sp_cur = sp_head;
-	for (x = 0; x < max_size && sp_cur != NULL; x++) {
+	for (x = 0; x < max_size && sp_cur != NULL; x++, sp_cur = sp_cur->less)
 		ar[x] = sp_cur->proc;
+
+
+	for (sp_cur = sp_head; sp_cur != NULL;) {
 		sp_tmp = sp_cur;
 		sp_cur = sp_cur->less;
 		free_sp(sp_tmp);