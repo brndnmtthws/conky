ef2855080ea1cccbd44a70cf2ca6840d3cce6f16
Pavel Labath <pavelo@centrum.sk>
2012-10-03 22:51:28 +0200

    Make sure we get report if one of the commands in the documentation pipeline fails

--- doc/docgen.sh
+++ doc/docgen.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl docs.xml > docs.html
-
-db2x_xsltproc -s man docs.xml -o docs.mxml
-db2x_manxml docs.mxml
-{ echo ".TH CONKY 1 \"August 2005\" \"conky compiled August 2005\" \"User Commands\""; sed 1d < conky.1; } > conky.2
-mv conky.2 conky.1
-#gzip --best --stdout conky.1 > conky.1.gz
-man ./conky.1 | col -b > README
+xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl docs.xml > docs.html && \
+db2x_xsltproc -s man docs.xml -o docs.mxml && \
+db2x_manxml docs.mxml && \
+{ echo ".TH CONKY 1 \"August 2005\" \"conky compiled August 2005\" \"User Commands\""; sed 1d < conky.1; } > conky.2 && \
+mv conky.2 conky.1 && \
+man ./conky.1 | col -b > README && \
 mv README ../
