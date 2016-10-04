2e4839e60126e53a33a00ad9a0bd431e6e22d0a4
Guido Falsi <madpilot78@users.noreply.github.com>
2016-10-04 09:47:27 +0200

    cur_x definition is wrapped inside #ifdef BUILD_X11, but here it is used outside of such ifdef. (#326)

--- src/conky.cc
+++ src/conky.cc
@@ -1814,8 +1814,8 @@ int draw_each_line_inner(char *s, int special_index, int last_special_applied)
 #endif /* BUILD_X11 */
 				case GOTO:
 					if (current->arg >= 0) {
-						cur_x = (int) current->arg;
 #ifdef BUILD_X11
+						cur_x = (int) current->arg;
 						//make sure shades are 1 pixel to the right of the text
 						if(draw_mode == BG) cur_x++;
 #endif /* BUILD_X11 */
