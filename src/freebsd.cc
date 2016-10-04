9047cee25e4e4b20b2b3a60b6d27ef94a00ed2a8
Guido Falsi <mad@madpilot.net>
2016-10-04 21:56:38 +0200

    Try more than one sysctl to get CPU temperature.
    Suggested by:	walter@pelissero.de
    Reference:	https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=210235

--- src/freebsd.cc
+++ src/freebsd.cc
@@ -426,13 +426,15 @@ double get_acpi_temperature(int fd)
 	int temp;
 	(void)fd;
 
-	if (GETSYSCTL("hw.acpi.thermal.tz0.temperature", temp)) {
-		fprintf(stderr,
-			"Cannot read sysctl \"hw.acpi.thermal.tz0.temperature\"\n");
-		return 0.0;
-	}
+	if (GETSYSCTL("hw.acpi.thermal.tz0.temperature", temp) == 0)
+		return KELVTOC(temp);
+	if (GETSYSCTL("dev.cpu.0.temperature", temp) == 0)
+		return KELVTOC(temp);
+	if (GETSYSCTL("dev.amdtemp.0.core0.sensor0", temp) == 0)
+		return KELVTOC(temp);
+	fprintf(stderr, "Cannot get temperature from sysctl\n");
 
-	return KELVTOC(temp);
+	return 0.0;
 }
 
 static void get_battery_stats(int *battime, int *batcapacity, int *batstate, int *ac) {
