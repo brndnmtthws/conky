562c7375883445acb58388ff16d9eb126b7e12a1
Brenden Matthews <brenden@diddyinc.com>
2018-01-23 13:52:11 -0500

    Fix segfault when out_to_x = false. (#455)
    This resolves #454.

--- src/conky.cc
+++ src/conky.cc
@@ -2076,20 +2076,24 @@ static void main_loop(void)
 	sigaddset(&newmask, SIGUSR1);
 #endif
 
+#ifdef BUILD_X11
 #ifdef BUILD_XSHAPE
-	/* allow only decorated windows to be given mouse input */
-	int major_version, minor_version;
-	if (!XShapeQueryVersion(display, &major_version, &minor_version)) {
-		NORM_ERR("Input shapes are not supported");
-	} else {
-		if (own_window.get(*state) &&
-		    (own_window_type.get(*state) != TYPE_NORMAL ||
-		     (TEST_HINT(own_window_hints.get(*state), HINT_UNDECORATED)))) {
-			XShapeCombineRectangles(display, window.window, ShapeInput, 0, 0,
-			   NULL, 0, ShapeSet, Unsorted);
+	if (out_to_x.get(*state)) {
+		/* allow only decorated windows to be given mouse input */
+		int major_version, minor_version;
+		if (!XShapeQueryVersion(display, &major_version, &minor_version)) {
+			NORM_ERR("Input shapes are not supported");
+		} else {
+			if (own_window.get(*state) &&
+					(own_window_type.get(*state) != TYPE_NORMAL ||
+					 (TEST_HINT(own_window_hints.get(*state), HINT_UNDECORATED)))) {
+				XShapeCombineRectangles(display, window.window, ShapeInput, 0, 0,
+						NULL, 0, ShapeSet, Unsorted);
+			}
 		}
 	}
 #endif /* BUILD_XSHAPE */
+#endif /* BUILD_X11 */
 
 	last_update_time = 0.0;
 	next_update_time = get_time() - fmod(get_time(), active_update_interval());
