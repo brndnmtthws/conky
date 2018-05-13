476b70a2d38f530fcde97e20014f46efc51c5c8a
Brenden Matthews <brenden@diddyinc.com>
2018-05-12 21:08:53 -0400

    Only run tidy on modified code, relocate scripts.

--- apply-clang-format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find . -iname "*.h" -o -iname "*.cc" -o -iname "*.hh" \
-  | xargs clang-format \
-    -style=file -i -fallback-style=google
--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -4,6 +4,8 @@ set(DO_CLANG_TIDY
   "${CLANG_TIDY_BIN}"
   -format-style='{BasedOnStyle: google, IndentWidth: 2}'
   -checks=*,-clang-analyzer-alpha.*
+  -fix
+  -fix-errors
 )
 
 if(CLANG_TIDY_BIN)
