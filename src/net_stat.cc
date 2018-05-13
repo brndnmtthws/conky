ad8d3814ab77aca2577934cc2533e2a86a7c220b
su8 <wifiextender2@gmail.com>
2018-05-13 17:05:38 +0200

    Use parentheses around addrs

--- src/net_stat.cc
+++ src/net_stat.cc
@@ -250,7 +250,7 @@ void print_addrs(struct text_object *obj, char *p, int p_max_size) {
 
   if (!ns) return;
 
-  if (NULL != ns->*addrs && strlen(ns->addrs) > 2) {
+  if (NULL != ns->(*addrs) && strlen(ns->addrs) > 2) {
     ns->addrs[strlen(ns->addrs) - 2] = 0; /* remove ", " from end of string */
     strncpy(p, ns->addrs, p_max_size);
   } else {
