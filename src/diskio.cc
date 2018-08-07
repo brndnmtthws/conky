4f3431e1056b05391a6de270004fc79aeca2f3b8
su8 <wifiextender2@gmail.com>
2018-08-07 17:53:57 +0200

    diskio.cc: Restore the multiply of 1024LL

--- src/diskio.cc
+++ src/diskio.cc
@@ -147,7 +147,7 @@ static void print_diskio_dir(struct text_object *obj, int dir, char *p,
 
   /* TODO: move this correction from kB to kB/s elsewhere
    * (or get rid of it??) */
-  human_readable(0, obj->data.s, (val / active_update_interval()), p, p_max_size);
+  human_readable(0, obj->data.s, (val / active_update_interval()) * 1024LL, p, p_max_size);
 }
 
 void print_diskio(struct text_object *obj, char *p, int p_max_size) {
