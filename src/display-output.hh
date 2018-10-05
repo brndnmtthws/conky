7e1a5bf3fa838651d4dede9edfdbb31647cb957d
François Revol <revol@free.fr>
2018-10-05 05:08:34 +0200

    Actually allow more than one display-output at once
    We already do this like with HTTP+stdout.

--- src/display-output.hh
+++ src/display-output.hh
@@ -86,10 +86,9 @@ protected:
 };
 
 /*
- * The selected and active display output.
- * XXX: do we want to support multiple outputs???
+ * The selected and active display outputs.
  */
-extern display_output_base *active_display_output;
+extern std::vector<display_output_base *> active_display_outputs;
 
 /*
  * Use this to declare a display output that has been disabled during compilation.
