21b4a0a403a01f297bfc446b22f98097eca30b7c
Guido Falsi <mad@madpilot.net>
2019-01-08 00:16:56 +0100

    These changes are required to make it build on FreeBSD i386 (x86/32)
    Otherwise I get this error while building:
    
    /wrkdirs/usr/ports/sysutils/conky/work/conky-1.11.1/src/core.cc:1503:50: error: assigning to 'void (*)(struct text_object *, char *, unsigned int)' from incompatible type 'void (*)(struct text_object *, char *, int)': type mismatch at 3rd parameter ('unsigned int' vs 'int')
      END OBJ(apm_adapter, 0) obj->callbacks.print = &print_apm_adapter;
                                                     ^~~~~~~~~~~~~~~~~~
    /wrkdirs/usr/ports/sysutils/conky/work/conky-1.11.1/src/core.cc:1504:55: error: assigning to 'void (*)(struct text_object *, char *, unsigned int)' from incompatible type 'void (*)(struct text_object *, char *, int)': type mismatch at 3rd parameter ('unsigned int' vs 'int')
      END OBJ(apm_battery_life, 0) obj->callbacks.print = &print_apm_battery_life;
                                                          ^~~~~~~~~~~~~~~~~~~~~~~
    /wrkdirs/usr/ports/sysutils/conky/work/conky-1.11.1/src/core.cc:1505:55: error: assigning to 'void (*)(struct text_object *, char *, unsigned int)' from incompatible type 'void (*)(struct text_object *, char *, int)': type mismatch at 3rd parameter ('unsigned int' vs 'int')
      END OBJ(apm_battery_time, 0) obj->callbacks.print = &print_apm_battery_time;
                                                          ^~~~~~~~~~~~~~~~~~~~~~~
    3 errors generated.
    *** [src/CMakeFiles/conky.dir/core.cc.o] Error code 1

--- src/bsdapm.h
+++ src/bsdapm.h
@@ -29,8 +29,8 @@
 #ifndef _BSDAPM_H
 #define _BSDAPM_H
 
-void print_apm_adapter(struct text_object *, char *, int);
-void print_apm_battery_life(struct text_object *, char *, int);
-void print_apm_battery_time(struct text_object *, char *, int);
+void print_apm_adapter(struct text_object *, char *, unsigned int);
+void print_apm_battery_life(struct text_object *, char *, unsigned int);
+void print_apm_battery_time(struct text_object *, char *, unsigned int);
 
 #endif /* _BSDAPM_H */
