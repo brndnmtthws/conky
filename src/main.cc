298eb76434cc2a62c76df706ff01ccd9c77a4681
Brenden Matthews <brenden@diddyinc.com>
2019-01-12 09:41:30 -0500

    Include missing FreeBSD header.

--- src/main.cc
+++ src/main.cc
@@ -43,6 +43,10 @@
 #include "ccurl_thread.h"
 #endif /* BUILD_CURL */
 
+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+#include "freebsd.h"
+#endif /* FreeBSD */
+
 #ifdef BUILD_BUILTIN_CONFIG
 #include "defconfig.h"
 
