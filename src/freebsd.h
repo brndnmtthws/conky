1fc2b17eb5c93ea7d0c00e6e7448fc9bc69aa276
Brenden Matthews <brenden@diddyinc.com>
2019-01-13 12:02:55 -0500

    FreeBSD build fix.

--- src/freebsd.h
+++ src/freebsd.h
@@ -41,6 +41,8 @@
 #include <machine/apm_bios.h>
 #endif /* i386 || __i386__ */
 
+extern kvm_t *kd;
+
 int get_entropy_avail(unsigned int *);
 int get_entropy_poolsize(unsigned int *);
 void print_sysctlbyname(struct text_object *, char *, unsigned int);
