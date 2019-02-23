bc29814fec15bde286f0cdde88efd14e58629f55
Brenden Matthews <brenden@diddyinc.com>
2019-02-22 21:51:32 -0500

    Bump version.

--- cmake/Conky.cmake
+++ cmake/Conky.cmake
@@ -117,7 +117,7 @@ endif(OS_SOLARIS)
 # Do version stuff
 set(VERSION_MAJOR "1")
 set(VERSION_MINOR "11")
-set(VERSION_PATCH "3")
+set(VERSION_PATCH "4")
 
 find_program(APP_AWK awk)
 if(NOT APP_AWK)
