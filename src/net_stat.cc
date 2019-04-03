0b4a40e06cc6764c7f45260981dd16d722f0c789
Brenden Matthews <brenden@diddyinc.com>
2019-04-03 08:17:53 -0400

    Increase default number of net interfaces.
    This should resolve the issue noted in #564.

--- src/net_stat.cc
+++ src/net_stat.cc
@@ -54,7 +54,7 @@
 #include "linux.h"
 #else
 static char e_iface[50] = "empty";
-static char interfaces_arr[64][64] = {""};
+static char interfaces_arr[MAX_NET_INTERFACES][64] = {""};
 #endif /* __linux__ */
 
 /* network interface stuff */
