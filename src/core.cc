faef8a4bf4b32795bbeb6d7053151c298c7e0003
helmat <helmat@users.noreply.github.com>
2020-01-06 06:41:55 +0100

    Fix if_mounted & if_running variables for FreeBSD

--- src/core.cc
+++ src/core.cc
@@ -1077,7 +1077,7 @@ struct text_object *construct_text_object(char *s, const char *arg, long line,
       obj->data.s = STRNDUP_ARG;
   obj->callbacks.iftest = &if_existing_iftest;
   obj->callbacks.free = &gen_free_opaque;
-#ifdef __linux__
+#if defined(__linux__) || defined(__FreeBSD__)
   END OBJ_IF_ARG(if_mounted, 0, "if_mounted needs an argument") obj->data.s =
       STRNDUP_ARG;
   obj->callbacks.iftest = &check_mount;
