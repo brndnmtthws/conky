56920fde12ebd36ac2f4213415e1f7a30d0ace82
isaac <isaac.nugroho@gmail.com>
2020-11-09 19:56:18 +0700

    add build option

--- src/conky.cc
+++ src/conky.cc
@@ -1451,8 +1451,13 @@ int draw_each_line_inner(char *s, int special_index, int last_special_applied) {
               unsigned long *tmpcolour = nullptr;
 
               if (current->last_colour != 0 || current->first_colour != 0) {
+#ifdef BUILD_HSV_GRADIENT
+                tmpcolour = do_hsv_gradient(w - 1, current->last_colour,
+                                        current->first_colour);
+#else
                 tmpcolour = do_gradient(w - 1, current->last_colour,
                                         current->first_colour);
+#endif
               }
               colour_idx = 0;
               for (i = w - 2; i > -1; i--) {
