9fda537b179e737416982adf5ba0ba5cd2b34139
François Revol <revol@free.fr>
2022-09-04 13:17:04 +0200

    Use another port for HTTP, one not blocked by Firefox and Chrome
    Fixes #1061.

--- cmake/ConkyBuildOptions.cmake
+++ cmake/ConkyBuildOptions.cmake
@@ -246,7 +246,7 @@ option(BUILD_IRC "Enable if you want IRC support" false)
 
 option(BUILD_HTTP "Enable if you want HTTP support" false)
 if(BUILD_HTTP)
-  set(HTTPPORT "10080" CACHE STRING "Port to use for out_to_http")
+  set(HTTPPORT "10090" CACHE STRING "Port to use for out_to_http")
 endif(BUILD_HTTP)
 
 option(BUILD_ICONV "Enable iconv support" false)
