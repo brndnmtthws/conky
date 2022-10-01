f39723fc1fd105157e2ba11709079d7f4ef5ab9d
Brenden Matthews <brenden@brndn.io>
2022-10-01 18:27:06 -0500

    Make search transitions less annoying.

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" className="theme-compiled">
+      <Html lang="en" className="theme-compiled scroll-smooth">
         <Head />
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
