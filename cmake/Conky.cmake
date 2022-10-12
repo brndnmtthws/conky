5879954cc05813ee16a1d615a1bb131cf834036f
Brenden Matthews <brenden@brndn.io>
2022-10-12 13:18:05 -0500

    Bump according to semver rules.

--- cmake/Conky.cmake
+++ cmake/Conky.cmake
@@ -124,8 +124,8 @@ endif(OS_SOLARIS)
 
 # Do version stuff
 set(VERSION_MAJOR "1")
-set(VERSION_MINOR "13")
-set(VERSION_PATCH "1")
+set(VERSION_MINOR "14")
+set(VERSION_PATCH "0")
 
 find_program(APP_AWK awk)
 if(NOT APP_AWK)
