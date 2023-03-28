42bcae0c0e76cdcaca2e021f5741312c4e0686ff
Brenden Matthews <brenden@brndn.io>
2023-03-28 08:21:17 -0400

    feat: Move docs to website, rearrange bits (#1475)

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html lang="en" className="theme-compiled scroll-smooth">
         <Head />
         <body
-          className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
+          className={`antialiased bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
           <Main />
           <NextScript />
