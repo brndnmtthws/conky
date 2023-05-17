796802b7a91c50fbb76c619128830fd75ff875e7
Guido Falsi <mad@madpilot.net>
2023-05-17 13:29:06 +0200

    Fix build on FreeBSD.
    The freq_sysctl varaible is being redefined with a different type.
    
    Update the first definition directly.

--- src/freebsd.cc
+++ src/freebsd.cc
@@ -544,14 +544,12 @@ void get_acpi_fan(char *p_client_buffer, size_t client_buffer_size) {
 char get_freq(char *p_client_buffer, size_t client_buffer_size,
               const char *p_format, int divisor, unsigned int cpu) {
   int freq;
-  char *freq_sysctl;
+  char freq_sysctl[16] = {0};
 
   if (!p_client_buffer || client_buffer_size <= 0 || !p_format ||
       divisor <= 0) {
     return 0;
   }
-
-  char freq_sysctl[16] = {0};
 
   snprintf(freq_sysctl, sizeof(freq_sysctl), "dev.cpu.%d.freq", (cpu - 1));
 
