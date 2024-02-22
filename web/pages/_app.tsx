aaee4ceb2fb8d5a05a884bd37ffc9daba813603d
Brenden Matthews <github@brenden.brndn.io>
2024-02-22 18:53:04 -0500

    Remove that

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '@fontsource-variable/fira-code'
 import '@fontsource-variable/inter'
 import '@fontsource-variable/newsreader'
-import 'prismjs/themes/prism-tomorrow.css'
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
 
