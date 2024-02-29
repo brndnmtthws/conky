2e8765f37034f8d409e481b59736082468b1ac33
Brenden Matthews <github@brenden.brndn.io>
2024-02-28 20:13:31 -0500

    Enable wayland in appimage build
    This resolves #1759.

--- appimage/build.sh
+++ appimage/build.sh
@@ -48,7 +48,7 @@ cmake -G Ninja                         \
   -DBUILD_NVIDIA=ON                    \
   -DBUILD_PULSEAUDIO=ON                \
   -DBUILD_RSS=ON                       \
-  -DBUILD_WAYLAND=OFF                  \
+  -DBUILD_WAYLAND=ON                   \
   -DBUILD_WLAN=ON                      \
   -DBUILD_X11=ON                       \
   -DBUILD_XMMS2=ON                     \
