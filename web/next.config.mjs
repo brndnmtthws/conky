a8e533ab9927aee9bb9822e61df17bd48dcaa583
Brenden Matthews <github@brenden.brndn.io>
2024-04-24 16:02:35 -0400

    Link to latest commit in web footer

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,13 +1,25 @@
 // @ts-check
+import { spawn } from 'child_process'
 
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  publicRuntimeConfig: {
-    modifiedDate: new Date().toISOString(),
-    modifiedYear: new Date().getFullYear(),
-  },
+const config = async (_phase, { _defaultConfig }) => {
+  const gitHash = await new Promise((resolve) => {
+    const git = spawn('git', ['rev-parse', '--short', 'HEAD'])
+    git.stdout.on('data', (data) => {
+      resolve(data.toString().trim())
+    })
+  })
+
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    publicRuntimeConfig: {
+      modifiedDate: new Date().toISOString(),
+      modifiedYear: new Date().getFullYear(),
+      gitHash,
+    },
+  }
+  return nextConfig
 }
 
-export default nextConfig
+export default config
