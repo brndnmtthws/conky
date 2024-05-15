c635df8689c66d53a34993f3ef3724a889e9d814
Fernando Apesteguía <fernando.apesteguia@gmail.com>
2024-05-15 20:18:11 +0200

    Fix build with specific option combination (#1918)
    Fixes incorrect member accessing left behind after #1910.

--- src/x11.cc
+++ src/x11.cc
@@ -1219,10 +1219,10 @@ void xdbe_swap_buffers() {
 void xpmdb_swap_buffers(void) {
   if (use_xpmdb.get(*state)) {
     XCopyArea(display, window.back_buffer, window.window, window.gc, 0, 0,
-              window.width, window.height, 0, 0);
+              window.geometry.get_width(), window.geometry.get_height(), 0, 0);
     XSetForeground(display, window.gc, 0);
-    XFillRectangle(display, window.drawable, window.gc, 0, 0, window.width,
-                   window.height);
+    XFillRectangle(display, window.drawable, window.gc, 0, 0, window.geometry.get_width(),
+                   window.geometry.get_height());
     XFlush(display);
   }
 }
