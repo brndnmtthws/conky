682db0c4df36e095d2da46f5305829b6634faf47
Fernando <fernando.apesteguia@gmail.com>
2024-05-19 20:03:05 +0200

    Fix build with specific option combination.
    With some option combination the code to be compiled has not been updated with
    regards to the changes in conky::rec, i.e get_height() becomes height() and
    get_width() becomes width().

--- src/x11-settings.cc
+++ src/x11-settings.cc
@@ -77,7 +77,7 @@ bool use_xpmdb_setting::set_up(lua::state &l) {
   if (!out_to_x.get(l)) return false;
 
   window.back_buffer =
-      XCreatePixmap(display, window.window, window.geometry.get_width() + 1, window.geometry.get_height() + 1,
+      XCreatePixmap(display, window.window, window.geometry.width() + 1, window.geometry.height() + 1,
                     DefaultDepth(display, screen));
   if (window.back_buffer != None) {
     window.drawable = window.back_buffer;
