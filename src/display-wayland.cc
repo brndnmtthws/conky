brndnmtthws/conky

commit 6adf6b9dd4d368640bf7ef57f3e6199d83154d78

Cleanup build flags, global namespace and includes (#1841)
- Separated displays so they don't have to all be included in build even when the features are disabled.
- Removed weird linker magic for display init and replaced it with straightforward (and faster) template functions that behave the same.
  - Replaced `disabled_display_output` classes with log messages.
- Add explicit compiler errors when feature dependent headers get included where they shouldn't (wl, x11).
- Switch BUILD_MOUSE_EVENTS dependency from OWN_WINDOW to BUILD_X11.
- Other minor improvements to some existing code which shouldn't affect behavior.
- Improve documentation.
- Remove X11 from unrelated headers.
  - This reaches 0% unneeded X11 polution in the sources.
  - Reenabled parts of window_type which would work in Wayland.
  - Remove unneeded guards.
- Display files are now no longer compiled if their features are disabled.

Signed-off-by: Tin Švagelj <tin.svagelj@live.com>

src/display-wayland.cc
@@ -24,9 +24,8 @@
  *
  */
 
-#include <config.h>
+#include "display-wayland.hh"
 
-#ifdef BUILD_WAYLAND
 #include <wayland-client.h>
 // #include "wayland.h"
 #include <cairo.h>
@@ -44,23 +43,18 @@
 #include <wlr-layer-shell-client-protocol.h>
 #include <xdg-shell-client-protocol.h>
 
-#endif /* BUILD_WAYLAND */
-
 #include <cstdint>
 #include <iostream>
 #include <sstream>
 
 #include "conky.h"
-#include "display-wayland.hh"
+#include "display-output.hh"
 #include "gui.h"
 #include "llua.h"
 #include "logging.h"
-#ifdef BUILD_X11
-#include "x11.h"
-#endif
-#ifdef BUILD_WAYLAND
+
 #include "fonts.h"
-#endif
+
 #ifdef BUILD_MOUSE_EVENTS
 #include <array>
 #include <map>
@@ -68,9 +62,6 @@
 #endif
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-
-/* TODO: cleanup global namespace */
-#ifdef BUILD_WAYLAND
 
 static int set_cloexec_or_close(int fd) {
   long flags;
@@ -238,24 +229,15 @@ static void wayland_create_window() {
   update_text_area(); /* to get initial size of the window */
 }
 
-#endif /* BUILD_WAYLAND */
-
 namespace conky {
 namespace {
-
-#ifdef BUILD_WAYLAND
 conky::display_output_wayland wayland_output;
-#else
-conky::disabled_display_output wayland_output_disabled("wayland",
-                                                       "BUILD_WAYLAND");
-#endif
-
 }  // namespace
-extern void init_wayland_output() {}
 
-namespace priv {}  // namespace priv
-
-#ifdef BUILD_WAYLAND
+template <>
+void register_output<output_t::WAYLAND>(display_outputs_t &outputs) {
+  outputs.push_back(&wayland_output);
+}
 
 display_output_wayland::display_output_wayland()
     : display_output_base("wayland") {
@@ -489,7 +471,11 @@ static void on_pointer_button(void *data, struct wl_pointer *pointer,
   size_t abs_y = w->rectangle.y + y;
 
   mouse_button_event event{
-      mouse_event_t::MOUSE_RELEASE,        x, y, abs_x, abs_y,
+      mouse_event_t::RELEASE,
+      x,
+      y,
+      abs_x,
+      abs_y,
       static_cast<mouse_button_t>(button),
   };
 
@@ -498,7 +484,7 @@ static void on_pointer_button(void *data, struct wl_pointer *pointer,
       // pass; default is MOUSE_RELEASE
       break;
     case WL_POINTER_BUTTON_STATE_PRESSED:
-      event.type = mouse_event_t::MOUSE_PRESS;
+      event.type = mouse_event_t::PRESS;
       break;
     default:
       return;
@@ -519,17 +505,17 @@ void on_pointer_axis(void *data, struct wl_pointer *pointer, std::uint32_t time,
   size_t abs_y = w->rectangle.y + y;
 
   mouse_scroll_event event{
-      x, y, abs_x, abs_y, scroll_direction_t::SCROLL_UP,
+      x, y, abs_x, abs_y, scroll_direction_t::UP,
   };
 
   switch (static_cast<wl_pointer_axis>(axis)) {
     case WL_POINTER_AXIS_VERTICAL_SCROLL:
-      event.direction = value > 0 ? scroll_direction_t::SCROLL_DOWN
-                                  : scroll_direction_t::SCROLL_UP;
+      event.direction =
+          value > 0 ? scroll_direction_t::DOWN : scroll_direction_t::UP;
       break;
     case WL_POINTER_AXIS_HORIZONTAL_SCROLL:
-      event.direction = value > 0 ? scroll_direction_t::SCROLL_RIGHT
-                                  : scroll_direction_t::SCROLL_LEFT;
+      event.direction =
+          value > 0 ? scroll_direction_t::RIGHT : scroll_direction_t::LEFT;
       break;
     default:
       return;
@@ -577,7 +563,9 @@ bool display_output_wayland::initialize() {
 
   wl_display_roundtrip(global_display);
   if (wl_globals.layer_shell == nullptr) {
-    CRIT_ERR("Compositor doesn't support wlr-layer-shell-unstable-v1. Can't run conky.");
+    CRIT_ERR(
+        "Compositor doesn't support wlr-layer-shell-unstable-v1. Can't run "
+        "conky.");
   }
 
   struct wl_surface *surface =
@@ -705,48 +693,34 @@ bool display_output_wayland::main_loop_wait(double t) {
 
     /* update struts */
     if (changed != 0) {
-      int anchor = -1;
+      int anchor = 0;
 
       DBGP("%s", _(PACKAGE_NAME ": defining struts\n"));
       fflush(stderr);
 
-      switch (text_alignment.get(*state)) {
-        case TOP_LEFT:
-          anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP |
-                   ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
+      alignment text_align = text_alignment.get(*state);
+      switch (vertical_alignment(text_align)) {
+        case axis_align::START:
+          anchor |= ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP;
           break;
-        case TOP_RIGHT:
-          anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP |
-                   ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT;
+        case axis_align::END:
+          anchor |= ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM;
           break;
-        case TOP_MIDDLE: {
-          anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP;
+        default:
           break;
-        }
-        case BOTTOM_LEFT:
-          anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM |
-                   ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
-          break;
-        case BOTTOM_RIGHT:
-          anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM |
-                   ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT;
-          break;
-        case BOTTOM_MIDDLE: {
-          anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM;
-          break;
-        }
-        case MIDDLE_LEFT: {
-          anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
-          break;
-        }
-        case MIDDLE_RIGHT: {
-          anchor = ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT;
-          break;
-        }
-
-        case NONE:
-        case MIDDLE_MIDDLE: /* XXX What about these? */;
       }
+      switch (horizontal_alignment(text_align)) {
+        case axis_align::START:
+          anchor |= ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT;
+          break;
+        case axis_align::END:
+          anchor |= ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT;
+          break;
+        default:
+          break;
+      }
+      // middle anchor alignment is the default and requires no special
+      // handling.
 
       if (anchor != -1) {
         zwlr_layer_surface_v1_set_anchor(global_window->layer_surface, anchor);
@@ -939,8 +913,10 @@ void display_output_wayland::move_win(int x, int y) {
   // window.y = y;
   // TODO
 }
-
-int display_output_wayland::dpi_scale(int value) { return value; }
+template <typename T, typename>
+T display_output_wayland::dpi_scale(T value) {
+  return value;
+}
 
 void display_output_wayland::end_draw_stuff() {
   window_commit_buffer(global_window);
@@ -1291,7 +1267,5 @@ void window_get_width_height(struct window *window, int *w, int *h) {
   *w = window->rectangle.width;
   *h = window->rectangle.height;
 }
-
-#endif /* BUILD_WAYLAND */
 
 }  // namespace conky

.gitignore
@@ -11,11 +11,17 @@ doc/*.html
 Makefile
 CMakeCache.txt
 CMakeFiles
+cmake_install.cmake
+cmake_uninstall.cmake
+CPackConfig.cmake
+CPackSourceConfig.cmake
+
 data/convertconf.h
 data/defconfig.h
 *.so
 *.a
 /config.h
+/build.h
 
 # Compiler cache
 .cache

3rdparty/toluapp/.gitignore
@@ -1,1 +1,4 @@
 build/
+
+# ignore generated Makefile; keep stub
+Makefile

cmake/ConkyBuildOptions.cmake
@@ -193,8 +193,8 @@ dependent_option(BUILD_XSHAPE "Enable Xshape support" true
   "BUILD_X11" false
   "Xshape support requires X11")
 dependent_option(BUILD_XINPUT "Build Xinput 2 support" true
-  "BUILD_X11;BUILD_MOUSE_EVENTS" false
-  "Xinput 2 support requires X11 and BUILD_MOUSE_EVENTS enabled")
+  "BUILD_X11" false
+  "Xinput 2 support requires X11")
 
 # if we build with any GUI support
 if(BUILD_X11)
@@ -206,8 +206,8 @@ if(BUILD_WAYLAND)
 endif(BUILD_WAYLAND)
 
 dependent_option(BUILD_MOUSE_EVENTS "Enable mouse event support" true
-  "BUILD_WAYLAND OR OWN_WINDOW" false
-  "Mouse event support requires Wayland or OWN_WINDOW enabled")
+  "BUILD_WAYLAND OR BUILD_X11" false
+  "Mouse event support requires Wayland or X11 enabled")
 
 # Lua library options
 dependent_option(BUILD_LUA_CAIRO "Build cairo bindings for Lua" false
@@ -247,9 +247,11 @@ option(BUILD_IRC "Enable if you want IRC support" false)
 
 option(BUILD_HTTP "Enable if you want HTTP support" false)
 
-if(BUILD_HTTP)
+if(NOT BUILD_HTTP)
   set(HTTPPORT "10080" CACHE STRING "Port to use for out_to_http")
-endif(BUILD_HTTP)
+else(NOT BUILD_HTTP)
+  set(HTTPPORT "10080")
+endif(NOT BUILD_HTTP)
 
 option(BUILD_ICONV "Enable iconv support" false)
 

data/conky-dfly-smp.conf
@@ -10,7 +10,10 @@
 --
 -- For more on Lua, see:
 -- https://www.lua.org/pil/contents.html
+--
+-- Conky Lua API: https://conky.cc/lua
 
+-- Configuration settings: https://conky.cc/config_settings
 conky.config = {
     alignment = 'bottom_left',
     background = false,
@@ -36,7 +39,8 @@ conky.config = {
     extra_newline = false,
     own_window = false,
     own_window_class = 'Conky',
-    own_window_type = 'desktop',
+    own_window_type = 'normal',
+    own_window_hints = 'undecorated,sticky,below,skip_taskbar,skip_pager',
     stippled_borders = 0,
     update_interval = 3.0,
     uppercase = false,
@@ -45,6 +49,7 @@ conky.config = {
     show_graph_range = false
 }
 
+-- Variables: https://conky.cc/variables
 conky.text = [[
 $sysname $version on $machine
 $hr

data/conky.conf
@@ -10,7 +10,10 @@
 --
 -- For more on Lua, see:
 -- https://www.lua.org/pil/contents.html
+--
+-- Conky Lua API: https://conky.cc/lua
 
+-- Configuration settings: https://conky.cc/config_settings
 conky.config = {
     alignment = 'top_left',
     background = false,
@@ -39,7 +42,8 @@ conky.config = {
     out_to_x = true,
     own_window = true,
     own_window_class = 'Conky',
-    own_window_type = 'desktop',
+    own_window_type = 'normal',
+    own_window_hints = 'undecorated,sticky,below,skip_taskbar,skip_pager',
     show_graph_range = false,
     show_graph_scale = false,
     stippled_borders = 0,
@@ -49,6 +53,7 @@ conky.config = {
     use_xft = true,
 }
 
+-- Variables: https://conky.cc/variables
 conky.text = [[
 ${color grey}Info:$color ${scroll 32 Conky $conky_version - $sysname $nodename $kernel $machine}
 $hr

data/conky_no_x11.conf
@@ -10,7 +10,10 @@
 --
 -- For more on Lua, see:
 -- https://www.lua.org/pil/contents.html
+--
+-- Conky Lua API: https://conky.cc/lua
 
+-- Configuration settings: https://conky.cc/config_settings
 conky.config = {
 	background = false,
 	cpu_avg_samples = 2,
@@ -22,6 +25,7 @@ conky.config = {
 	use_spacer = 'none',
 };
 
+-- Variables: https://conky.cc/variables
 conky.text = 
 [[${scroll 16 $nodename - $sysname $kernel on $machine | }
 Uptime: $uptime

doc/config_settings.yaml
@@ -27,10 +27,38 @@ desc: |-
 values:
   - name: alignment
     desc: |-
-      Aligned position on screen, may be top_left, top_right,
-      top_middle, bottom_left, bottom_right, bottom_middle, middle_left,
-      middle_middle, middle_right, or none (also can be abbreviated as tl,
-      tr, tm, bl, br, bm, ml, mm, mr). See also gap_x and gap_y.
+      Aligned position on screen, may be `none` or one of:
+
+       <table>
+        <tr>
+          <td><code>top_left</code> (<em>or</em> <code>tl</code>)</td>
+          <td><code>top_middle</code> (<em>or</em> <code>tm</code>)</td>
+          <td><code>top_right</code> (<em>or</em> <code>tr</code>)</td>
+        </tr>
+        <tr>
+          <td><code>middle_left</code> (<em>or</em> <code>ml</code>)</td>
+          <td><code>middle_middle</code> (<em>or</em> <code>mm</code>)</td>
+          <td><code>middle_right</code> (<em>or</em> <code>mr</code>)</td>
+        </tr>
+        <tr>
+          <td><code>bottom_left</code> (<em>or</em> <code>bl</code>)</td>
+          <td><code>bottom_middle</code> (<em>or</em> <code>bm</code>)</td>
+          <td><code>bottom_right</code> (<em>or</em> <code>br</code>)</td>
+        </tr>
+      </table>
+
+      In case of `panel` and `dock` windows, it might make more sense to use one
+      of the following aliases:
+
+      | Alias  |     Value     |
+      |:------:|:-------------:|
+      | `top`  | `top_middle`  |
+      | `left` | `middle_left` |
+      |`right` |`middle_right` |
+      |`bottom`|`bottom_middle`|
+      |`center`|`middle_middle`|
+
+      See also `gap_x` and `gap_y` settings.
   - name: append_file
     desc: Append the file given as argument.
   - name: background
@@ -437,7 +465,7 @@ values:
     default: false
   - name: own_window_type
     desc: |-
-      If own_window is set, under X11 you can specify type of window conky
+      If `own_window` is set, under X11 you can specify type of window conky
       displayed as:
 
       - `normal` mode makes conky show as normal window. This mode can be
@@ -464,6 +492,7 @@ values:
         panels/windows as those will cover conky.
         `own_window_hints` are ignored for `override` windows.
 
+      To make conky mount on root window, set `own_window` to `false`.
     default: normal
     args:
       - (normal|desktop|dock|panel|utility|override)

commit 808397b981b016e80df727da47bc7cc559c5cd45

Fix out_to_wayland giving nothing
This fix a problem caused by 6adf6b9dd4d368640bf7ef57f3e6199d83154d78:

The display outputs are sorted by priority on src/display-output.cc.

Before the cleanup commit, Wayland was choosed instead of X11. But
no thanks to any explicit code. Just because outputs was inserted
luckily in the correct order, before the sort.


Because out_to_x is true, and out_to_wayland is false by default,
it means the users have to enable explicitely wayland on their config.

We should rather prefer Wayland because it need explicit configuration.


To do so, I think we should define that Wayland priority surpass the X11
one, instead of fixing the register_output order.

src/display-wayland.cc
@@ -243,7 +243,7 @@ void register_output<output_t::WAYLAND>(display_outputs_t &outputs) {
 display_output_wayland::display_output_wayland()
     : display_output_base("wayland") {
   is_graphical = true;
-  priority = 2;
+  priority = 3;
 }
 
 bool display_output_wayland::detect() {