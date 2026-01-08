d71fdcfdbac4a5fb459eb5f02630b60e174cb842
Brenden Matthews <github@brenden.brndn.io>
2026-01-08 08:12:40 -0500

    fix(lua): initialize default harfbuzz language
    Ensure fallback language assignment when Lua callers omit "language".
    
    Applies to both cairo_text_hp_show and cairo_text_hp_text_size to
    prevent uninitialized hb_language_t usage.

--- lua/libcairo_text_helper.h
+++ lua/libcairo_text_helper.h
@@ -25,8 +25,8 @@
 #ifndef _LIBCAIRO_TEXT_HELPER_H_
 #define _LIBCAIRO_TEXT_HELPER_H_
 
-#include <cairo/cairo.h>
 #include <cairo/cairo-ft.h>
+#include <cairo/cairo.h>
 
 #include <fontconfig/fontconfig.h>
 
@@ -37,9 +37,9 @@
 #include <freetype/ftsnames.h>
 #include <freetype/tttables.h>
 
-#include <harfbuzz/hb.h>
 #include <harfbuzz/hb-ft.h>
 #include <harfbuzz/hb-glib.h>
+#include <harfbuzz/hb.h>
 
 #define max_fonts 4096
 #define max_font_name_len 4096
@@ -54,9 +54,9 @@ typedef struct _FontData {
   /* Stored here so they can be freed later */
   FT_Library ft_library;
   FT_Face ft_face;
-}FontData;
+} FontData;
 
-FontData* font_cache[max_fonts] = {NULL};
+FontData *font_cache[max_fonts] = {NULL};
 
 typedef enum _cairo_text_alignment {
   CAIRO_TEXT_ALIGN_LEFT = 0,
@@ -65,116 +65,114 @@ typedef enum _cairo_text_alignment {
 } cairo_text_alignment_t;
 
 /* Imports a font from a file */
-FontData *cairo_text_hp_import_font(const char *font, int font_size)
-{
+FontData *cairo_text_hp_import_font(const char *font, int font_size) {
   /* Fontconfig will take a font name and return the file with */
   /* the best match as Freetype only works directly with font files */
-  
+
   FcInit();
-  FcConfig* config = FcInitLoadConfigAndFonts();
+  FcConfig *config = FcInitLoadConfigAndFonts();
 
   // crash here
   if (!font) {
-    printf("Error: cairo_text font not set.\n"); 
-    return NULL;
-  }   
-  FcPattern* pat = FcNameParse((const FcChar8*)font);
-  FcConfigSubstitute(config, pat, FcMatchPattern);
-  FcDefaultSubstitute(pat);
-    
-  FcResult result;
-  FcPattern* font_pat = FcFontMatch(config, pat, &result);
-
-  /* Will be freed with font */
-  FcChar8* file = NULL; 
-  FcPatternGetString(font_pat, FC_FILE, 0, &file);
-  
-  if (!file) {
-    /* FIXME: add error handling */
-    printf("Error: cairo_text couldn't find font.\n"); 
+    printf("Error: cairo_text font not set.\n");
     return NULL;
   }
-  
+  FcPattern *pat = FcNameParse((const FcChar8 *)font);
+  FcConfigSubstitute(config, pat, FcMatchPattern);
+  FcDefaultSubstitute(pat);
+
+  FcResult result;
+  FcPattern *font_pat = FcFontMatch(config, pat, &result);
+
+  /* Will be freed with font */
+  FcChar8 *file = NULL;
+  FcPatternGetString(font_pat, FC_FILE, 0, &file);
+
+  if (!file) {
+    /* FIXME: add error handling */
+    printf("Error: cairo_text couldn't find font.\n");
+    return NULL;
+  }
+
   FontData *font_data = (FontData *)malloc(sizeof(struct _FontData));
 
   strncpy(font_data->name, font, max_font_name_len);
-  
+
   /* Load the font */
   FT_Init_FreeType(&font_data->ft_library);
   FT_Library_SetLcdFilter(font_data->ft_library, FT_LCD_FILTER_DEFAULT);
   FT_New_Face(font_data->ft_library, (char *)file, 0, &font_data->ft_face);
-  FT_Set_Char_Size(font_data->ft_face, font_size*64, font_size*64,0,0);
-  
+  FT_Set_Char_Size(font_data->ft_face, font_size * 64, font_size * 64, 0, 0);
+
   /* Store font data for use later */
-  font_data->cairo_ft_face = cairo_font_face_reference(cairo_ft_font_face_create_for_ft_face(font_data->ft_face, 0));
+  font_data->cairo_ft_face = cairo_font_face_reference(
+      cairo_ft_font_face_create_for_ft_face(font_data->ft_face, 0));
   font_data->hb_ft_font = hb_ft_font_create(font_data->ft_face, NULL);
   font_data->hb_ft_face = hb_ft_face_create(font_data->ft_face, NULL);
   font_data->font_size = font_size;
-  
+
   /* Cleanup font config */
   FcPatternDestroy(font_pat);
   FcPatternDestroy(pat);
   FcConfigDestroy(config);
   /* FIXME: Crashes */
   /*FcFini(); */
-  
+
   return font_data;
 }
 
 /* Either loads font from cache or imports from a file */
-FontData *cairo_text_hp_load_font(const char *font, int font_size)
-{
+FontData *cairo_text_hp_load_font(const char *font, int font_size) {
   FontData *font_data = NULL;
-  
+
   /* Search for font in cache */
   for (int i = 0; i < max_fonts; i++) {
-      if (font_cache[i] == NULL) {
-        /* Haven't used this font yet so load and cache it */
-        font_data = cairo_text_hp_import_font(font, font_size);
-        font_cache[i] = font_data;
-        return font_data;
-      }
-      if ((strncmp(font_cache[i]->name, font, max_font_name_len) == 0) &&
-         (font_size == font_cache[i]->font_size)) {
-        font_data = font_cache[i];
-        return font_data;
-      }
+    if (font_cache[i] == NULL) {
+      /* Haven't used this font yet so load and cache it */
+      font_data = cairo_text_hp_import_font(font, font_size);
+      font_cache[i] = font_data;
+      return font_data;
+    }
+    if ((strncmp(font_cache[i]->name, font, max_font_name_len) == 0) &&
+        (font_size == font_cache[i]->font_size)) {
+      font_data = font_cache[i];
+      return font_data;
+    }
   }
 
   return font_data;
 }
 
-void cairo_text_hp_destroy_font(FontData *font)
-{
+void cairo_text_hp_destroy_font(FontData *font) {
   cairo_font_face_destroy(font->cairo_ft_face);
-  cairo_font_face_destroy (font->cairo_ft_face);
+  cairo_font_face_destroy(font->cairo_ft_face);
   hb_font_destroy(font->hb_ft_font);
   hb_face_destroy(font->hb_ft_face);
-  
-  FT_Done_Face (font->ft_face);
+
+  FT_Done_Face(font->ft_face);
   FT_Done_FreeType(font->ft_library);
-  
+
   free(font);
 }
 
-void cairo_text_hp_delete_fonts()
-{
+void cairo_text_hp_delete_fonts() {
   for (int i = 0; (i < max_fonts) && (font_cache[i] != NULL); i++) {
     cairo_text_hp_destroy_font(font_cache[i]);
   }
 }
 
-/* 
+/*
  * Direction calls hb_direction_from_string example values are LTR and RTL
  *   https://harfbuzz.github.io/harfbuzz-hb-common.html#hb-direction-from-string
- * Script is an ISO 15924 4 character string, "Zyyy" can be used for "Common" and "Zinh"
- *  for "Inherited". 
+ * Script is an ISO 15924 4 character string, "Zyyy" can be used for "Common"
+ * and "Zinh" for "Inherited".
  *  https://harfbuzz.github.io/harfbuzz-hb-common.html#hb-script-from-string
  * Language is a BCP 47 language tag. eg "en" or "en-US"
  */
-void cairo_text_hp_show(cairo_t *cr, int x, int y, const char *text, const char *font, int font_size, 
-                   cairo_text_alignment_t alignment, const char *language, const char *script, const char *direction)
-{
+void cairo_text_hp_show(cairo_t *cr, int x, int y, const char *text,
+                        const char *font, int font_size,
+                        cairo_text_alignment_t alignment, const char *language,
+                        const char *script, const char *direction) {
   /* It seems that lua may just pass NULL for an empty string */
   if (text == NULL) {
     printf("Error: CairoTextHelper: TextShow: Null string\n");
@@ -197,33 +195,29 @@ void cairo_text_hp_show(cairo_t *cr, int x, int y, const char *text, const char 
     return;
   }
 
-  double x1,x2,y1,y2;
+  double x1, x2, y1, y2;
 
   cairo_clip_extents(cr, &x1, &y1, &x2, &y2);
 
-  int width = x2-x1;
-  int height = y2-y1;
+  int width = x2 - x1;
+  int height = y2 - y1;
 
   hb_language_t text_language;
   hb_direction_t text_direction;
   if (language != NULL) {
-    text_language = hb_language_from_string (language, -1);
-  }
-  else {
+    text_language = hb_language_from_string(language, -1);
+  } else {
     /* Use en as the default as if you are sharing configs with other */
     /* people, you don't want them to break if they use a different lang */
-    hb_language_from_string("en", -1);
+    text_language = hb_language_from_string("en", -1);
   }
 
   hb_script_t text_script = hb_script_from_string(script, -1);
-  if (text_script == HB_SCRIPT_UNKNOWN) {
-    text_script = HB_SCRIPT_COMMON;
-  }
+  if (text_script == HB_SCRIPT_UNKNOWN) { text_script = HB_SCRIPT_COMMON; }
 
   if (direction != NULL) {
     text_direction = hb_direction_from_string(direction, -1);
-  }
-  else {
+  } else {
     text_direction = hb_script_get_horizontal_direction(text_script);
   }
   /* Note Direction can be invalid if user passes something invalid */
@@ -231,18 +225,18 @@ void cairo_text_hp_show(cairo_t *cr, int x, int y, const char *text, const char 
   if (text_direction == HB_DIRECTION_INVALID) {
     text_direction = HB_DIRECTION_LTR;
   }
-  
+
   /* Draw text */
   /* Create a buffer for harfbuzz to use */
   hb_buffer_t *buf = hb_buffer_create();
 
-  //alternatively you can use hb_buffer_set_unicode_funcs(buf, hb_glib_get_unicode_funcs());
+  // alternatively you can use hb_buffer_set_unicode_funcs(buf,
+  // hb_glib_get_unicode_funcs());
   hb_buffer_set_unicode_funcs(buf, hb_glib_get_unicode_funcs());
   hb_buffer_set_language(buf, text_language);
   if (script != NULL) {
     hb_buffer_set_script(buf, text_script); /* see hb-common.h */
-  }
-  else {
+  } else {
     hb_buffer_guess_segment_properties(buf);
   }
   hb_buffer_set_direction(buf, text_direction); /* or LTR */
@@ -253,72 +247,69 @@ void cairo_text_hp_show(cairo_t *cr, int x, int y, const char *text, const char 
 
   /* Need to calculate the Baseline for drawing on the y axis */
   hb_font_extents_t font_extents;
-  hb_font_get_extents_for_direction(font_data->hb_ft_font, text_direction, &font_extents);
+  hb_font_get_extents_for_direction(font_data->hb_ft_font, text_direction,
+                                    &font_extents);
 
   /* Note Line Gap was always 0 in my testing */
-  int baseline_offset = font_extents.ascender/64 + 0.5 * font_extents.line_gap/64 + 1;
+  int baseline_offset =
+      font_extents.ascender / 64 + 0.5 * font_extents.line_gap / 64 + 1;
 
   /* Hand the layout to cairo to render */
-  unsigned int         glyph_count;
-  hb_glyph_info_t     *glyph_info   = hb_buffer_get_glyph_infos(buf, &glyph_count);
-  hb_glyph_position_t *glyph_pos    = hb_buffer_get_glyph_positions(buf, &glyph_count);
-  cairo_glyph_t       *cairo_glyphs = (cairo_glyph_t *)malloc(sizeof(cairo_glyph_t) * glyph_count);
+  unsigned int glyph_count;
+  hb_glyph_info_t *glyph_info = hb_buffer_get_glyph_infos(buf, &glyph_count);
+  hb_glyph_position_t *glyph_pos =
+      hb_buffer_get_glyph_positions(buf, &glyph_count);
+  cairo_glyph_t *cairo_glyphs =
+      (cairo_glyph_t *)malloc(sizeof(cairo_glyph_t) * glyph_count);
 
-  /* RTL positioning seems to be slightly off and characters don't link as they should */
-  /* This hack gets it significantly closer to correct but is not 100% for all fonts and sizes */
-  int rtl_fix = font_data->font_size/10;
+  /* RTL positioning seems to be slightly off and characters don't link as they
+   * should */
+  /* This hack gets it significantly closer to correct but is not 100% for all
+   * fonts and sizes */
+  int rtl_fix = font_data->font_size / 10;
 
   unsigned int string_width_in_pixels = 0;
-  for (int i=0; i < glyph_count; ++i) {
+  for (int i = 0; i < glyph_count; ++i) {
     if (HB_DIRECTION_IS_VERTICAL(text_direction)) {
-      int glyph_width = glyph_pos[i].x_offset/64*-1;
+      int glyph_width = glyph_pos[i].x_offset / 64 * -1;
       if (glyph_width > string_width_in_pixels) {
         string_width_in_pixels = glyph_width;
       }
-    }
-    else {
-      string_width_in_pixels += glyph_pos[i].x_advance/64;
+    } else {
+      string_width_in_pixels += glyph_pos[i].x_advance / 64;
       if (text_direction == HB_DIRECTION_RTL) {
         string_width_in_pixels -= rtl_fix;
       }
     }
   }
   /* More RTL Hacks */
-  if (text_direction == HB_DIRECTION_RTL) {
-    string_width_in_pixels += 2;
-  }
+  if (text_direction == HB_DIRECTION_RTL) { string_width_in_pixels += 2; }
   int draw_x = x;
   int draw_y = y;
 
   if (HB_DIRECTION_IS_VERTICAL(text_direction)) {
     if (alignment == CAIRO_TEXT_ALIGN_LEFT) {
       draw_x = x + string_width_in_pixels;
-    }
-    else if (alignment == CAIRO_TEXT_ALIGN_RIGHT) {
+    } else if (alignment == CAIRO_TEXT_ALIGN_RIGHT) {
       draw_x = width - string_width_in_pixels + x;
+    } else {
+      draw_x = width / 2 - string_width_in_pixels + x;
     }
-    else {
-      draw_x = width/2 - string_width_in_pixels + x;
-    }
-  }
-  else {
+  } else {
     draw_y = baseline_offset + y;
     if (alignment == CAIRO_TEXT_ALIGN_LEFT) {
       if (text_direction == HB_DIRECTION_RTL) {
         draw_x = width - x - string_width_in_pixels;
       }
       // LTR handled as default.
-    }
-    else if (alignment == CAIRO_TEXT_ALIGN_RIGHT) {
+    } else if (alignment == CAIRO_TEXT_ALIGN_RIGHT) {
       if (text_direction == HB_DIRECTION_RTL) {
         draw_x = x;
-      }
-      else {
+      } else {
         draw_x = width - x - string_width_in_pixels;
       }
-    }
-    else if (alignment == CAIRO_TEXT_ALIGN_CENTER) {
-      draw_x = width/2 - string_width_in_pixels/2 + x;
+    } else if (alignment == CAIRO_TEXT_ALIGN_CENTER) {
+      draw_x = width / 2 - string_width_in_pixels / 2 + x;
     }
   }
 
@@ -326,20 +317,19 @@ void cairo_text_hp_show(cairo_t *cr, int x, int y, const char *text, const char 
   x = 0;
   y = 0;
 
-  for (int i=0; i < glyph_count; ++i) {
-      cairo_glyphs[i].index = glyph_info[i].codepoint;
-      cairo_glyphs[i].x = x + draw_x + (glyph_pos[i].x_offset/64.0);
-      cairo_glyphs[i].y = y + draw_y - (glyph_pos[i].y_offset/64.0);
-      x += glyph_pos[i].x_advance/64.0;
-      if (text_direction == HB_DIRECTION_RTL) {
-        x -= rtl_fix;
-      }
-      y -= glyph_pos[i].y_advance/64.0;
+  for (int i = 0; i < glyph_count; ++i) {
+    cairo_glyphs[i].index = glyph_info[i].codepoint;
+    cairo_glyphs[i].x = x + draw_x + (glyph_pos[i].x_offset / 64.0);
+    cairo_glyphs[i].y = y + draw_y - (glyph_pos[i].y_offset / 64.0);
+    x += glyph_pos[i].x_advance / 64.0;
+    if (text_direction == HB_DIRECTION_RTL) { x -= rtl_fix; }
+    y -= glyph_pos[i].y_advance / 64.0;
   }
 
   cairo_set_font_face(cr, font_data->cairo_ft_face);
   cairo_set_font_size(cr, font_data->font_size);
-  // Use glyph path and fill_preserve so its possible to add borders etc from lua after
+  // Use glyph path and fill_preserve so its possible to add borders etc from
+  // lua after
   cairo_glyph_path(cr, cairo_glyphs, glyph_count);
   cairo_fill_preserve(cr);
 
@@ -347,10 +337,9 @@ void cairo_text_hp_show(cairo_t *cr, int x, int y, const char *text, const char 
   hb_buffer_destroy(buf);
 }
 
-void cairo_text_hp_text_size( const char *text, const char *font, int font_size, 
-                             const char *language, const char *script, const char *direction, 
-                             int *width, int *height)
-{
+void cairo_text_hp_text_size(const char *text, const char *font, int font_size,
+                             const char *language, const char *script,
+                             const char *direction, int *width, int *height) {
   /* It seems that lua may just pass NULL for an empty string */
   if (text == NULL) {
     printf("Error: CairoTextHelper: TextSize: Null string\n");
@@ -376,23 +365,19 @@ void cairo_text_hp_text_size( const char *text, const char *font, int font_size,
   hb_language_t text_language;
   hb_direction_t text_direction;
   if (language != NULL) {
-    text_language = hb_language_from_string (language, -1);
-  }
-  else {
+    text_language = hb_language_from_string(language, -1);
+  } else {
     /* Use en as the default as if you are sharing configs with other */
     /* people, you don't want them to break if they use a different lang */
-    hb_language_from_string("en", -1);
+    text_language = hb_language_from_string("en", -1);
   }
 
   hb_script_t text_script = hb_script_from_string(script, -1);
-  if (text_script == HB_SCRIPT_UNKNOWN) {
-    text_script = HB_SCRIPT_COMMON;
-  }
+  if (text_script == HB_SCRIPT_UNKNOWN) { text_script = HB_SCRIPT_COMMON; }
 
   if (direction != NULL) {
     text_direction = hb_direction_from_string(direction, -1);
-  }
-  else {
+  } else {
     text_direction = hb_script_get_horizontal_direction(text_script);
   }
   /* Note Direction can be invalid if user passes something invalid */
@@ -400,33 +385,37 @@ void cairo_text_hp_text_size( const char *text, const char *font, int font_size,
   if (text_direction == HB_DIRECTION_INVALID) {
     text_direction = HB_DIRECTION_LTR;
   }
-  
+
   /* Draw text */
   /* Create a buffer for harfbuzz to use */
   hb_buffer_t *buf = hb_buffer_create();
 
-  //alternatively you can use hb_buffer_set_unicode_funcs(buf, hb_glib_get_unicode_funcs());
+  // alternatively you can use hb_buffer_set_unicode_funcs(buf,
+  // hb_glib_get_unicode_funcs());
   hb_buffer_set_unicode_funcs(buf, hb_glib_get_unicode_funcs());
   hb_buffer_set_direction(buf, text_direction); /* or LTR */
   if (script != NULL) {
     hb_buffer_set_script(buf, text_script); /* see hb-unicode.h */
-  }
-  else {
+  } else {
     hb_buffer_guess_segment_properties(buf);
-  }  hb_buffer_set_language(buf, text_language);
+  }
+  hb_buffer_set_language(buf, text_language);
 
   /* Layout the text */
   hb_buffer_add_utf8(buf, text, strlen(text), 0, strlen(text));
   hb_shape(font_data->hb_ft_font, buf, NULL, 0);
 
   hb_font_extents_t font_extents;
-  hb_font_get_extents_for_direction(font_data->hb_ft_font, text_direction, &font_extents);
+  hb_font_get_extents_for_direction(font_data->hb_ft_font, text_direction,
+                                    &font_extents);
 
   /* Hand the layout to cairo to render */
-  unsigned int         glyph_count;
-  hb_glyph_info_t     *glyph_info   = hb_buffer_get_glyph_infos(buf, &glyph_count);
-  hb_glyph_position_t *glyph_pos    = hb_buffer_get_glyph_positions(buf, &glyph_count);
-  cairo_glyph_t       *cairo_glyphs = (cairo_glyph_t *)malloc(sizeof(cairo_glyph_t) * glyph_count);
+  unsigned int glyph_count;
+  hb_glyph_info_t *glyph_info = hb_buffer_get_glyph_infos(buf, &glyph_count);
+  hb_glyph_position_t *glyph_pos =
+      hb_buffer_get_glyph_positions(buf, &glyph_count);
+  cairo_glyph_t *cairo_glyphs =
+      (cairo_glyph_t *)malloc(sizeof(cairo_glyph_t) * glyph_count);
 
   unsigned int string_width_in_pixels = 0;
   unsigned int string_height_in_pixels = 0;
@@ -434,26 +423,28 @@ void cairo_text_hp_text_size( const char *text, const char *font, int font_size,
   /* Temp variable used for each glyph */
   hb_glyph_extents_t glyph_extents;
   if (text_direction == HB_DIRECTION_LTR ||
-      text_direction == HB_DIRECTION_RTL)
-  {
+      text_direction == HB_DIRECTION_RTL) {
     /* Width */
-    for (int i=0; i < glyph_count; ++i) {
-        string_width_in_pixels += glyph_pos[i].x_advance/64+glyph_pos[i].x_offset/64;
-        hb_font_get_glyph_extents (font_data->hb_ft_font, glyph_info[i].codepoint,
-                                   &glyph_extents);
-        int h = ((glyph_extents.height/64)*-1)+(glyph_extents.y_bearing/64);
-        if (h > string_height_in_pixels) {
-          string_height_in_pixels = h;
-        }
+    for (int i = 0; i < glyph_count; ++i) {
+      string_width_in_pixels +=
+          glyph_pos[i].x_advance / 64 + glyph_pos[i].x_offset / 64;
+      hb_font_get_glyph_extents(font_data->hb_ft_font, glyph_info[i].codepoint,
+                                &glyph_extents);
+      int h =
+          ((glyph_extents.height / 64) * -1) + (glyph_extents.y_bearing / 64);
+      if (h > string_height_in_pixels) { string_height_in_pixels = h; }
     }
     /* Height */
-    //string_height_in_pixels = font_extents.ascender/64 - font_extents.descender/64;
+    // string_height_in_pixels = font_extents.ascender/64 -
+    // font_extents.descender/64;
   } else {
     /* Width */
-    string_width_in_pixels = font_extents.ascender/64 - font_extents.descender/64;
+    string_width_in_pixels =
+        font_extents.ascender / 64 - font_extents.descender / 64;
     /* Height */
-    for (int i=0; i < glyph_count; ++i) {
-        string_height_in_pixels += glyph_pos[i].y_advance/64+glyph_pos[i].y_offset/64;
+    for (int i = 0; i < glyph_count; ++i) {
+      string_height_in_pixels +=
+          glyph_pos[i].y_advance / 64 + glyph_pos[i].y_offset / 64;
     }
   }
 
