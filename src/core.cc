e83878c728b51363d0aca815951e5bd7e202f87e
Mathieu Fluhr <76774136+mfluhr@users.noreply.github.com>
2026-06-01 13:46:25 +0200

    Added a new width argument to 'hr' and 'stippled_hr' variables (#2315)
    * Added a new width argument to 'hr' and 'stippled_hr' variables
    
    * fix: avoid hr cleanup regressions
    
    ---------
    
    Co-authored-by: Brenden Matthews <github@brenden.brndn.io>

--- src/core.cc
+++ src/core.cc
@@ -1016,8 +1016,7 @@ struct text_object *construct_text_object(char *s, const char *arg, long line,
   END OBJ(fs_used, &update_fs_stats) init_fs(obj, arg);
   obj->callbacks.print = &print_fs_used;
 #ifdef BUILD_GUI
-  END OBJ(hr, nullptr) obj->data.l =
-      arg != nullptr ? strtol(arg, nullptr, 10) : 1;
+  END OBJ(hr, nullptr) scan_hr(obj, arg);
   obj->callbacks.print = &new_hr;
 #endif /* BUILD_GUI */
   END OBJ(nameserver, &update_dns_data) parse_nameserver_arg(obj, arg);
@@ -1057,7 +1056,8 @@ struct text_object *construct_text_object(char *s, const char *arg, long line,
   END OBJ_ARG(hwmon, 0, "hwmon needs arguments") parse_hwmon_sensor(obj, arg);
   obj->callbacks.print = &print_sysfs_sensor;
   obj->callbacks.free = &free_sysfs_sensor;
-  END OBJ_ARG(hwmonbar, 0, "hwmonbar needs arguments") parse_hwmon_bar(obj, arg);
+  END OBJ_ARG(hwmonbar, 0, "hwmonbar needs arguments")
+      parse_hwmon_bar(obj, arg);
   obj->callbacks.barval = &sysfs_sensor_barval;
   obj->callbacks.free = &free_sysfs_sensor;
 #endif /* __linux__ */
